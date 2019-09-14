5d9e7e58b7f5a8903fd88037597ddafb9df0e7c3
Pavel Zhukov <cray0000@gmail.com>
2019-09-14 10:34:57 +0300

    [template] init sharedb. Add interactive example

--- template/App.js
+++ template/App.js
@@ -1,14 +1,39 @@
+import 'startupjs/init'
 import React, { memo } from 'react'
 import {
   View,
-  Text
+  Text,
+  TouchableOpacity
 } from 'react-native'
+import {
+  observer,
+  useDoc,
+  $root
+} from 'startupjs'
 
 import './App.styl'
 
-export default function App () {
+export default observer(function App () {
+  let [counter, $counter] = useDoc('service', 'counter')
+
+  if (!counter) throw new Promise(resolve => {
+    $root.add('service', { id: 'counter', value: 0 }, resolve)
+  })
+
+  async function increment () {
+    $counter.increment('value', 1)
+  }
+
+  async function decrement () {
+    $counter.increment('value', -1)
+  }
+
   return pug`
     View.body
-      Text.greeting Hello World
+      Text.greeting Hello World. Counter: #{counter && counter.value}
+      TouchableOpacity.increment(onPress=increment)
+        Text.label +
+      TouchableOpacity.decrement(onPress=decrement)
+        Text.label -
   `
-}
+})
