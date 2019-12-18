6cdcebe7668948c282b01afe7a1566af2320c5b3
tohellandback <ivangitmail@gmail.com>
2019-12-18 18:45:17 +0300

    [ui] add Shadow component

--- styleguide/main/pages/PComponent/index.js
+++ styleguide/main/pages/PComponent/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { observer } from 'startupjs'
 import { Props } from 'components'
+import Shadow from '../../../../packages/ui/components/Shadow'
 import { View, Text } from 'react-native'
 import propTypes from 'prop-types'
 import './index.styl'
@@ -12,6 +13,18 @@ export default observer(function PComponent () {
         Text.text Syntax highlighter
       View.right
         Props(of=RenderText)
+    Shadow.testShadowS
+      View(style={height: 100, width: 100, alignItems: 'center', justifyContent: 'center'})
+        Text Shadow S
+    Shadow.testShadowM(variant='m')
+      View(style={height: 100, width: 100, alignItems: 'center', justifyContent: 'center'})
+        Text Shadow M
+    Shadow.testShadowM(variant='l')
+      View(style={height: 100, width: 100, alignItems: 'center', justifyContent: 'center'})
+        Text Shadow L
+    Shadow.testShadowM(variant='xl')
+      View(style={height: 100, width: 100, alignItems: 'center', justifyContent: 'center'})
+        Text Shadow XL
   `
 })
 
