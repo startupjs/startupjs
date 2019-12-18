ad9b388a5bd3c9c68c3d52218115b32ec6270074
estatix <statix.411@ya.ru>
2019-12-18 16:06:34 +0300

    [ui] add button component

--- packages/ui/components/Button/index.js
+++ packages/ui/components/Button/index.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import { View, TouchableOpacity, Text } from 'react-native'
 import { observer } from 'startupjs'
-import { Text } from 'react-native'
+import PropTypes from 'prop-types'
+import './index.styl'
 
-export default observer(function Button ({
-  style
-}) {
+const Button = observer(({
+  style,
+  children,
+  variant = 'flat', // shadowed, outlined, ghost
+  size = 'normal', // large, big
+  squared, // bool
+  icon,
+  disabled,
+  onPress,
+  ...props
+}) => {
+  let PressContainer = onPress ? TouchableOpacity : View
+
   return pug`
-    Text Button component
+    PressContainer.root(
+      onPress=disabled ? undefined : onPress
+      activeOpacity=onPress && 1
+      styleName=[variant, size, {
+        squared,
+        icon,
+        disabled
+      }]
+      style=style
+      ...props
+    )
+      if icon
+        View.icon(style={width: 15, height: 15, backgroundColor: '#ffae00'})
+      if children
+        Text.text= children
   `
 })
+
+Button.propType = {
+  variant: PropTypes.oneOf(['flat', 'shadowed', 'outlined', 'ghost']),
+  size: PropTypes.oneOf(['normal', 'large', 'big']),
+  squared: PropTypes.bool,
+  disabled: PropTypes.bool
+}
+
+export default Button
