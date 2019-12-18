b221b8fc41a55e754c987e69001e5bf4aa26e9c5
estatix <statix.411@ya.ru>
2019-12-18 14:47:06 +0300

    [ui] refactor Typography component

--- packages/ui/components/Typography/index.js
+++ packages/ui/components/Typography/index.js
@@ -3,15 +3,17 @@ import { Text, Platform } from 'react-native'
 import { observer } from 'startupjs'
 import './index.styl'
 
-function generateTag (tag) {
+function generateTag (tag, ariaLevelNumber) {
   return observer(
     ({ bold, children, style, ...props }) => {
-      const isNative = Platform.OS !== 'web'
-      const Tag = isNative ? Text : tag
+      const isWeb = Platform.OS === 'web'
+      const role = isWeb && ariaLevelNumber ? { accessibilityRole: 'heading', 'aria-level': ariaLevelNumber } : {}
+
       return pug`
-        Tag.root(
+        Text.root(
           styleName=[tag, { bold }]
           style=style
+          ...role
           ...props
         )= children
       `
@@ -19,12 +21,12 @@ function generateTag (tag) {
   )
 }
 
-export const H1 = generateTag('h1')
-export const H2 = generateTag('h2')
-export const H3 = generateTag('h3')
-export const H4 = generateTag('h4')
-export const H5 = generateTag('h5')
-export const H6 = generateTag('h6')
-export const Normal = generateTag('Text')
-export const Description = generateTag('Text')
-export const Small = generateTag('Text')
+export const H1 = generateTag('h1', 1)
+export const H2 = generateTag('h2', 2)
+export const H3 = generateTag('h3', 3)
+export const H4 = generateTag('h4', 4)
+export const H5 = generateTag('h5', 5)
+export const H6 = generateTag('h6', 6)
+export const Span = generateTag('normal')
+export const Description = generateTag('description')
+export const Small = generateTag('small')
