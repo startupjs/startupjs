f89af34a568b823155f302b3dbee625c1057a7e6
tohellandback <ivangitmail@gmail.com>
2019-12-18 18:45:44 +0300

    [ui] add Shadow component

---
+++ packages/ui/components/Shadow/index.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import Div from '../Div'
+import { observer } from 'startupjs'
+import './index.styl'
+
+const SHADOWS = {
+  s: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 2.22,
+
+    elevation: 3
+  },
+  m: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 4.65,
+
+    elevation: 6
+  },
+  l: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 6.27,
+
+    elevation: 10
+  },
+  xl: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 8
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 10.32,
+
+    elevation: 16
+  }
+}
+
+export default observer(function Shadow ({
+  style,
+  children,
+  variant = 's' // s, m, l, xl
+}) {
+  return pug`
+    Div.root(
+      ...SHADOWS[variant]
+      style=[style]
+      styleName=[variant]
+    )
+      = children
+
+  `
+})
