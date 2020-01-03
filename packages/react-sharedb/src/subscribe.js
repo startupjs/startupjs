ec9060eaa0b1551a5d71a8f06ed916aa12f9b2b7
Pavel Zhukov <cray0000@gmail.com>
2020-01-03 12:57:33 +0200

    [react-sharedb] Refactor. Move racer and sharedb monkey patches into a separate file. Prevent tree shaking on files which are used purely for their side effects (globalInit and patchRacer).

--- packages/react-sharedb/src/subscribe.js
+++ packages/react-sharedb/src/subscribe.js
@@ -4,7 +4,6 @@ import union from 'lodash/union'
 import _keys from 'lodash/keys'
 import isEqual from 'lodash/isEqual'
 // import debounce from 'lodash/debounce'
-import racer from 'racer'
 import React from 'react'
 import hoistStatics from 'hoist-non-react-statics'
 import model from '@startupjs/model'
@@ -15,18 +14,12 @@ import Local from './types/Local'
 import Value from './types/Value'
 import Api from './types/Api'
 import batching from './batching'
-import RacerLocalDoc from 'racer/lib/Model/LocalDoc'
-import RacerRemoteDoc from 'racer/lib/Model/RemoteDoc'
-import RacerUtil from 'racer/lib/util'
-import RacerQuery from 'racer/lib/Model/Query'
-import SharedbDoc from 'sharedb/lib/client/doc'
 import semaphore from './semaphore'
 import { isExtraQuery } from './isExtraQuery'
 import {
   observe,
   unobserve,
-  observable,
-  isObservable
+  observable
 } from '@nx-js/observer-util'
 
 const STORE = 'store'
@@ -398,194 +391,4 @@ function bindMethods (object, methodsToBind) {
 
 function getScopedModelName (key) {
   return `$${key}`
-}
-
-// ----------------------------------------------
-//   Monkey patches of ShareDB and Racer
-// ----------------------------------------------
-
-// Patch applying sharedb operations to prevent extra rerender from triggering
-const oldHandleOp = SharedbDoc.prototype._handleOp
-SharedbDoc.prototype._handleOp = function () {
-  let value
-  batching.batch(() => {
-    value = oldHandleOp.apply(this, arguments)
-  })
-  return value
-}
-
-const BATCH_SETTERS = ['_mutate', '_setEach', '_setDiff', '_setDiffDeep']
-
-for (let methodName of BATCH_SETTERS) {
-  const oldMethod = racer.Model.prototype[methodName]
-  racer.Model.prototype[methodName] = function () {
-    let value
-    batching.batch(() => {
-      value = oldMethod.apply(this, arguments)
-    })
-    return value
-  }
-}
-
-const WARNING_SETTERS = ['_set', '_setDiff', '_setNull', '_del']
-for (let methodName of WARNING_SETTERS) {
-  const oldMethod = racer.Model.prototype[methodName]
-  racer.Model.prototype[methodName] = function (segments) {
-    if (
-      segments.length === 2 &&
-      segments[0] === DEFAULT_COLLECTION &&
-      !semaphore.allowComponentSetter
-    ) {
-      throw new Error(
-        `You can't use '${methodName.replace(/^_/, '')}' on component's ` +
-          `${$STORE} root path. Use 'setEach' instead.`
-      )
-    }
-    return oldMethod.apply(this, arguments)
-  }
-}
-
-// Monkey patch racer's local documents to be observable
-let oldUpdateCollectionData = RacerLocalDoc.prototype._updateCollectionData
-RacerLocalDoc.prototype._updateCollectionData = function () {
-  // Only objects and arrays can be made observable (both are typeof 'object')
-  if (this.data && typeof this.data === 'object') {
-    this.data = observable(this.data)
-  }
-  if (
-    !semaphore.ignoreCollectionObservableWarning &&
-    !isObservable(this.collectionData) &&
-    this.collectionName !== '$connection'
-  ) {
-    console.warn(
-      `[react-sharedb] Local collection "${this
-        .collectionName}" is not initialized to be observable. ` +
-        `Run require("react-sharedb").initLocalCollection("${this
-          .collectionName}") before using it anywhere. ` +
-        `You must also do it right after cleaning it up with model.silent().destroy("${this
-          .collectionName}")`
-    )
-  }
-  return oldUpdateCollectionData.apply(this, arguments)
-}
-
-// Monkey patch racer remote doc to make document observable when it's created locally
-let oldRemoteDocUpdateCollectionData =
-  RacerRemoteDoc.prototype._updateCollectionData
-RacerRemoteDoc.prototype._updateCollectionData = function () {
-  if (this.shareDoc.data) this.shareDoc.data = observable(this.shareDoc.data)
-  return oldRemoteDocUpdateCollectionData.apply(this, arguments)
-}
-
-// Monkey patch Racer for additional stuff
-
-racer.Model.prototype.fetchSync = function () {
-  var _resolve
-  var promise = new Promise(function (resolve) { _resolve = resolve })
-  this._forSubscribable(arguments, 'fetch', _resolve, promise)
-  return promise
-}
-
-racer.Model.prototype.subscribeSync = function () {
-  var _resolve
-  var promise = new Promise(function (resolve) { _resolve = resolve })
-  this._forSubscribable(arguments, 'subscribe', _resolve, promise)
-  return promise
-}
-
-// resolve is used for SYNC mode.
-// In this mode the subscribeSync funcion retuns a promise
-// which is gonna be either:
-//   - resolved, if we are already subscribed to all data (it's in racer model)
-//   - pending, if at least one subscription needs to be executed
-racer.Model.prototype._forSubscribable = function (argumentsObject, method, resolve, promise) {
-  var args, cb
-  if (!argumentsObject.length) {
-    // Use this model's scope if no arguments
-    args = [null]
-  } else if (typeof argumentsObject[0] === 'function') {
-    // Use this model's scope if the first argument is a callback
-    args = [null]
-    cb = argumentsObject[0]
-  } else if (Array.isArray(argumentsObject[0])) {
-    // Items can be passed in as an array
-    args = argumentsObject[0]
-    cb = argumentsObject[1]
-  } else {
-    // Or as multiple arguments
-    args = Array.prototype.slice.call(argumentsObject)
-    var last = args[args.length - 1]
-    if (typeof last === 'function') cb = args.pop()
-  }
-
-  // [SYNC MODE] For sync usage of subscribe
-  if (resolve) {
-    cb = function () {
-      promise.sync = true
-      resolve()
-    }
-  }
-
-  var group = RacerUtil.asyncGroup(this.wrapCallback(cb))
-  var finished = group()
-  var docMethod = method + 'Doc'
-
-  this.root.connection.startBulk()
-  for (var i = 0; i < args.length; i++) {
-    var item = args[i]
-    if (item instanceof RacerQuery) {
-      item[method](group())
-    } else {
-      var segments = this._dereference(this._splitPath(item))
-      if (segments.length === 2) {
-        // Do the appropriate method for a single document.
-        this[docMethod](segments[0], segments[1], group())
-      } else {
-        var message = 'Cannot ' + method + ' to path: ' + segments.join('.')
-        group()(new Error(message))
-      }
-    }
-  }
-  this.root.connection.endBulk()
-
-  // [SYNC MODE] Don't force async execution if we are in sync mode
-  if (resolve) return finished()
-
-  process.nextTick(finished)
-}
-
-// Monkey patch query subscribe to return data synchronously if it's in cache
-RacerQuery.prototype.subscribe = function (cb) {
-  cb = this.model.wrapCallback(cb)
-  this.model._context.subscribeQuery(this)
-
-  if (this.subscribeCount++) {
-    var query = this
-    // Synchronously return data if it's already in cache
-    // process.nextTick(function() {
-    var data = query.model._get(query.segments)
-    if (data) {
-      cb()
-    } else {
-      query._pendingSubscribeCallbacks.push(cb)
-    }
-    // })
-    return this
-  }
-
-  if (!this.created) this.create()
-
-  var options = (this.options) ? RacerUtil.copy(this.options) : {}
-  options.results = this._getShareResults()
-
-  // When doing server-side rendering, we actually do a fetch the first time
-  // that subscribe is called, but keep track of the state as if subscribe
-  // were called for proper initialization in the client
-  if (this.model.root.fetchOnly) {
-    this._shareFetchedSubscribe(options, cb)
-  } else {
-    this._shareSubscribe(options, cb)
-  }
-
-  return this
 }
