909091a1b1c694f82c2c85b46bf801ae82935852
simonprod <xeywar@gmail.com>
2020-01-09 11:30:41 +0300

    [styleguide] add transform imports plugin

--- styleguide/babel.config.js
+++ styleguide/babel.config.js
@@ -1,10 +1,28 @@
 const getConfig = require('startupjs/bundler').babelConfig
 
+const noImportConversion = (importName, isBool) => {
+  const headers = new Set(['H1', 'H2', 'H3', 'H4', 'H5', 'H6'])
+  if (headers.has(importName)) return isBool ? true : '/Headers'
+  return isBool ? false : `/${importName}`
+}
+
 module.exports = function (api) {
   const config = getConfig(api, {
     alias: {
       ui: '@startupjs/ui'
     }
   })
+
+  config.plugins = [
+    ...config.plugins,
+    ['@gzaripov/babel-plugin-transform-imports', {
+      '@startupjs/ui': {
+        transform: importName => `@startupjs/ui/components${noImportConversion(importName)}`,
+        skipDefaultConversion: importName => noImportConversion(importName, true),
+        preventFullImport: true
+      }
+    }]
+  ]
+
   return config
 }
