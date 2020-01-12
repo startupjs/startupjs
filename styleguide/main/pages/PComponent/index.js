1a8d3f0b831fc4c4c636b3acf95082779a865286
estatix <statix.411@ya.ru>
2020-01-12 14:12:43 +0300

    [ui] progress wip

--- styleguide/main/pages/PComponent/index.js
+++ styleguide/main/pages/PComponent/index.js
@@ -1,19 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { observer } from 'startupjs'
-// import { Props } from 'components'
-import { Progress } from '@startupjs/ui'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { Props } from 'components'
+import { View, Text } from 'react-native'
 import propTypes from 'prop-types'
 import './index.styl'
 
 export default observer(function PComponent () {
-  let [kek, kekUp] = useState(0.5344343)
-  console.log(kek)
   return pug`
-    View(style={width: 300})
-      Progress(value=kek label='Loading' total=100)
-      TouchableOpacity(onPress=()=>kekUp(kek + 0.5))
-        Text(style={marginTop: 20}) UP
+    View.root
+      View.left
+        Text.text Syntax highlighter
+      View.right
+        Props(of=RenderText)
   `
 })
 
