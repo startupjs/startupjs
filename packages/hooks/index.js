a9a6426da6531da42a53438986eb75e5332f324d
tohellandback <ivangitmail@gmail.com>
2020-01-13 15:22:53 +0300

    [startupjs] add hooks package

---
+++ packages/hooks/index.js
@@ -0,0 +1,1 @@
+export { default as useSyncEffect } from './lib/useSyncEffect'
---
+++ packages/hooks/lib/useSyncEffect.js
@@ -0,0 +1,22 @@
+import { useLayoutEffect, useMemo, useRef } from 'react'
+
+export default function useSyncEffect (fn, inputs) {
+  let destroy = useRef()
+
+  const destroyFn = () => {
+    if (destroy.current) {
+      typeof destroy.current === 'function' && destroy.current()
+      delete destroy.current
+    }
+  }
+
+  useMemo(() => {
+    destroyFn()
+    destroy.current = fn()
+  }, inputs)
+
+  // final destroy when component gets unmounted
+  useLayoutEffect(() => {
+    return destroyFn
+  }, [])
+}
