b2944811614db936ce01221761243bd8d8b32194
Pavel Turchik <pturchik@yandex.ru>
2020-01-14 23:56:32 +0300

    [ui] refactor Br

--- packages/ui/components/Br/index.js
+++ packages/ui/components/Br/index.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import { observer } from 'startupjs'
 import { Text } from 'react-native'
+import PropTypes from 'prop-types'
 import { u } from './../../config/helpers'
 import './index.styl'
 const LINE_HEIGHT = u(2)
 
-export default observer(({ half, lines = 1 }) => {
+function Br ({ half, lines }) {
   const height = half ? LINE_HEIGHT / 2 : LINE_HEIGHT * lines
   return pug`
     Text.root(style={height})
   `
-})
+}
+
+Br.defaultProps = {
+  half: false,
+  lines: 1
+}
+
+Br.propTypes = {
+  half: PropTypes.boolean,
+  lines: PropTypes.number
+
+}
+
+export default observer(Br)
