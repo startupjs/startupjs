79a6c2d058c541cc5068c16a2ecbde952286e8cf
tohellandback <ivangitmail@gmail.com>
2020-01-14 21:22:11 +0300

    [Sidebar] add animation on open/close. [hooks] add useDidUpdate hook.

--- styleguide/main/pages/PComponent/index.js
+++ styleguide/main/pages/PComponent/index.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { observer } from 'startupjs'
 import { Props } from 'components'
 import { View, Text } from 'react-native'
 import propTypes from 'prop-types'
-import { SmartSidebar } from 'ui'
+import { SmartSidebar, Div } from 'ui'
 import './index.styl'
 
 export default observer(function PComponent () {
+  const [open, setOpen] = useState()
+  function renderContent () {
+    return pug`
+      Div(onPress=() => setOpen(!open))
+        Text ToggleSidebar
+    `
+  }
   return pug`
-    SmartSidebar
+    SmartSidebar(renderContent=renderContent open=open)
+      Div.div(onPress=() => setOpen(!open) shadow='m')
+        Text ToggleSidebar
       View.root
         View.left
           Text.text Syntax highlighter
