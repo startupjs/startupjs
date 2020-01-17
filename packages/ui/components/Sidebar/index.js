ab6635d8057a3699ed799684efc9297b10588e56
tohellandback <ivangitmail@gmail.com>
2020-01-17 16:41:15 +0300

    [ui -> Sidebar] refactor animation

--- packages/ui/components/Sidebar/index.js
+++ packages/ui/components/Sidebar/index.js
@@ -6,7 +6,6 @@ import PropTypes from 'prop-types'
 import Div from '../Div'
 import config from '../../config/rootConfig'
 import './index.styl'
-
 function Sidebar ({
   backgroundColor,
   children,
@@ -19,13 +18,14 @@ function Sidebar ({
   const componentId = useComponentId()
 
   const [open] = useSession(path || `Sidebar.${componentId}`)
+  const [invisible, setInvisible] = useState(!open)
 
-  const [animation] = useState(new Animated.Value(open ? width : 0))
+  const [animation] = useState(new Animated.Value(open ? 0 : -width))
+  const [contentAnimation] = useState(new Animated.Value(open ? width : 0))
   const _renderContent = () => {
     return pug`
       ScrollView(
-        style={width}
-        contentContainerStyle={flexGrow: 1}
+        contentContainerStyle={ flex: 1 }
       )
         = renderContent()
     `
@@ -33,31 +33,59 @@ function Sidebar ({
 
   useDidUpdate(() => {
     if (open) {
-      Animated.timing(
-        animation,
-        {
-          toValue: width,
-          duration: 250
-        }
-      ).start()
+      setInvisible(false)
+      Animated.parallel([
+        Animated.timing(
+          contentAnimation,
+          {
+            toValue: width,
+            duration: 250
+          }
+        ),
+        Animated.timing(
+          animation,
+          {
+            toValue: 0,
+            duration: 250
+          }
+        )
+      ]).start()
     } else {
-      Animated.timing(
-        animation,
-        {
-          toValue: 0,
-          duration: 200
-        }
-      ).start()
+      Animated.parallel([
+        Animated.timing(
+          contentAnimation,
+          {
+            toValue: 0,
+            duration: 200
+          }
+        ),
+        Animated.timing(
+          animation,
+          {
+            toValue: -width,
+            duration: 200
+          }
+        )
+      ]).start(() => {
+        setInvisible(true)
+      })
     }
   }, [!!open])
 
   return pug`
     Div.root(styleName=[position])
-      Div(shadow='s')
-        Animated.View(
-          style={flex: 1, width: animation, overflow: 'hidden'}
-        )= _renderContent()
-      Div.main= children
+      Animated.View.sidebar(
+        style={[position]: animation, width}
+        styleName={invisible}
+      )
+        Div(level=1 style={flex: 1})
+          = _renderContent()
+      - const propName = 'padding' + position[0].toUpperCase() + position.slice(1)
+      Animated.View.main(
+        style={
+          [propName]: contentAnimation
+        }
+      )= children
   `
 }
 
