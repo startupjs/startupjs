170244a0e6dd4c8c1acf11be531ef9353d2f6f13
Pavel Turchik <pturchik@yandex.ru>
2020-01-21 16:53:47 +0300

    [app] refactor routes

---
+++ packages/app/client/Router/RoutesWrapper.js
@@ -0,0 +1,15 @@
+import Stack from 'react-router-native-stack'
+const DEFAULT_ANIMATE = false
+// Support for web https://github.com/Traviskn/react-router-native-stack/pull/47
+
+export default function RoutesWrapper ({
+  animate = DEFAULT_ANIMATE,
+  children
+}) {
+  return pug`
+    if animate
+      Stack(gestureEnabled=false animationType='slide-horizontal')= children
+    else
+      = children
+  `
+}
