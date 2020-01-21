fd5d8f6abfcbb3ea7bc0f61a189d205a4300ed9f
tohellandback <ivangitmail@gmail.com>
2020-01-21 23:52:38 +0300

    [ui -> Button] add icons

--- packages/ui/components/Button/index.js
+++ packages/ui/components/Button/index.js
@@ -6,26 +6,59 @@ import { observer } from 'startupjs'
 import propTypes from 'prop-types'
 import config from '../../config/rootConfig'
 import './index.styl'
+// TODO. Remove
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+
+const { colors } = config
 
 const SHADOWS = config.shadows
+
+const SIZES = {
+  normal: 'normal',
+  large: 'large',
+  big: 'big'
+}
+
+const VARIANTS = {
+  flat: 'flat',
+  outlined: 'outlined',
+  ghost: 'ghost',
+  shadowed: 'shadowed'
+}
+
+const ICON_SIZES = {
+  normal: 's',
+  large: 'm',
+  big: 'l'
+}
+
+const ICON_COLORS = {
+  flat: colors.white,
+  outlined: colors.primary,
+  ghost: colors.dark,
+  shadowed: colors.dark
+}
 
 function Button ({
   style,
   children,
   variant,
+  circle,
   size,
   squared,
   disabled,
   icon,
-  iconType,
-  iconSize,
+  iconProps,
+  rightIcon,
+  rightIconProps,
   iconColor,
   label,
   level,
   onPress,
   ...props
 }) {
-  let _level = variant === 'shadowed' ? level || 1 : level
+  const _level = variant === 'shadowed' ? level || 1 : level
+  const isSingleIcon = !!icon && !label
 
   return pug`
     Div.root(
@@ -35,8 +68,9 @@ function Button ({
         variant,
         disabled ? variant + '-disabled' : '',
         {
+          icon: isSingleIcon,
           squared,
-          icon
+          circle
         }
       ]
       level=_level
@@ -44,8 +78,13 @@ function Button ({
       onPress=onPress
       ...props
     )
-      if icon
-        Icon.icon(name=icon type=iconType size=iconSize color=iconColor)
+      if !!icon
+        Icon.leftIcon(
+          styleName={single: isSingleIcon}
+          icon=icon
+          size=iconProps.size || ICON_SIZES[size]
+          color=iconProps.color || variant === VARIANTS.outlined && disabled ? colors.darkLighter : ICON_COLORS[variant]
+        )
       if label
         Span.label(
           bold
@@ -55,6 +94,12 @@ function Button ({
             disabled ? variant + '-disabled' : ''
           ]
         )= label
+      if !!rightIcon
+        Icon.rightIcon(
+          icon=rightIcon
+          size=rightIconProps.size || ICON_SIZES[size]
+          color=rightIconProps.color || variant === VARIANTS.outlined && disabled ? colors.darkLighter : ICON_COLORS[variant]
+        )
   `
 }
 
@@ -66,18 +111,28 @@ Button.defaultProps = {
   type: 'primary',
 
   // TODO. remove
-  onPress: () => null
+  onPress: () => null,
+  icon: faStar,
+  // rightIcon: faStar,
+  iconProps: {},
+  rightIconProps: {}
+}
+
+const iconsPropTypes = {
+  height: propTypes.number,
+  width: propTypes.number,
+  size: propTypes.oneOf(['xs', 's', 'm', 'l', 'xl', 'xxl']),
+  color: propTypes.string
 }
 
 Button.propTypes = {
-  variant: propTypes.oneOf(['flat', 'outlined', 'ghost', 'shadowed']),
-  size: propTypes.oneOf(['normal', 'large', 'big']),
+  variant: propTypes.oneOf(Object.values(VARIANTS)),
+  size: propTypes.oneOf(Object.values(SIZES)),
+  circle: propTypes.bool,
   squared: propTypes.bool,
+  iconProps: propTypes.shape(iconsPropTypes),
+  rightIconProps: propTypes.shape(iconsPropTypes),
   disabled: propTypes.bool,
-  icon: propTypes.string,
-  iconType: propTypes.string,
-  iconSize: propTypes.oneOf(['xs', 's', 'm', 'l', 'xl', 'xxl']),
-  iconColor: propTypes.string,
   label: propTypes.string,
   level: propTypes.oneOf(SHADOWS.map((item, index) => index)),
   onPress: propTypes.func.isRequired
