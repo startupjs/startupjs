847fc691550db8bc4ad62dc07f0f82c4ae0bbfaf
tohellandback <ivangitmail@gmail.com>
2020-02-03 13:55:47 +0300

    [ui -> Pagination] change pagination api

--- packages/ui/components/Pagination/index.js
+++ packages/ui/components/Pagination/index.js
@@ -7,17 +7,30 @@ import Span from '../Span'
 import './index.styl'
 
 function Pagination ({
-  count,
+  buttonsCount,
+  showLastPage,
+  showFirstPage,
   value,
   total,
   limit,
   onChange
 }) {
+  const page = value + 1
   const pagesCount = Math.ceil(total / limit)
-  const from = value - (count + 1) < 0
-    ? 0 : value - (count + 1)
-  const to = (value + count) * limit > total
-    ? pagesCount : value + count
+
+  const from = page - buttonsCount < 0
+    ? 0
+    : page + Math.ceil(buttonsCount / 2) > pagesCount
+      ? pagesCount - buttonsCount
+      : page - Math.ceil(buttonsCount / 2)
+
+  const to = pagesCount < buttonsCount
+    ? pagesCount
+    : page - buttonsCount < 0
+      ? buttonsCount
+      : page + Math.floor(buttonsCount / 2) > pagesCount
+        ? pagesCount
+        : Math.floor(buttonsCount / 2) + page
 
   return pug`
     Div.root(level=1)
@@ -25,10 +38,10 @@ function Pagination ({
         styleName={ disabled: value <= 1 }
         bold
         label='back'
-        disabled=value <= 1
+        disabled=page <= 1
         onPress=() => onChange(value - 1)
       )
-      if from
+      if from && showFirstPage
         PaginationButton(
           label=1
           onPress=() => onChange(1)
@@ -37,43 +50,50 @@ function Pagination ({
           Span ...
       each item, index in Array(to - from).fill(from)
         - const page = from + index + 1
+        - const isActive = page === value + 1
         PaginationButton(
           key=index
-          active=page === value
+          active=isActive
           label=page
-          onPress=() => onChange(page)
+          onPress=() => onChange(page - 1)
         )
-      if to < pagesCount
+      if to < pagesCount && showLastPage
         Div(style={justifyContent: 'flex-end'})
           Span ...
         PaginationButton(
           label=pagesCount
           disabled=value >= pagesCount
-          onPress=() => onChange(pagesCount)
+          onPress=() => onChange(pagesCount - 1)
         )
       PaginationButton.next(
         styleName={ disabled: value >= pagesCount }
         bold
         label='next'
-        disabled=value >= pagesCount
+        disabled=page >= pagesCount
         onPress=() => onChange(value + 1)
       )
   `
 }
 
 Pagination.defaultProps = {
-  count: 2,
-  total: 1800,
+  buttonsCount: 5,
+  total: 400,
   limit: 10,
+  value: 1,
+  showLastPage: false,
+  showFirstPage: false,
 
   // TODO. remove
   onChange: (page) => console.log(page)
 }
 
 Pagination.propTypes = {
-  count: propTypes.number,
-  value: propTypes.number,
-  limit: propTypes.number,
+  buttonsCount: propTypes.number,
+  showLastPage: propTypes.bool,
+  showFirstPage: propTypes.bool,
+  total: propTypes.number.isRequired,
+  value: propTypes.number.isRequired,
+  limit: propTypes.number.isRequired,
   onChange: propTypes.func.isRequired
 }
 
@@ -97,7 +117,7 @@ function PaginationButton ({
 }
 
 export default observer(function Test () {
-  const [page, setPage] = useState(1)
+  const [page, setPage] = useState(0)
   return pug`
     Span= page
     Pagination(
