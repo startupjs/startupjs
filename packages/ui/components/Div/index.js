d82136467eeea8bb3542c4548b6db90a3e5b3a72
tohellandback <ivangitmail@gmail.com>
2020-02-06 22:11:05 +0300

    [ui -> Div] add background onpacity on hover

--- packages/ui/components/Div/index.js
+++ packages/ui/components/Div/index.js
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
 import propTypes from 'prop-types'
 import { TouchableOpacity, View } from 'react-native'
 import { observer } from 'startupjs'
 import config from '../../config/rootConfig'
+import alpha from 'color-alpha'
 import './index.styl'
+
+const { opacity } = config
 
 const SHADOWS = config.shadows
 
 function Div ({
   style,
   children,
+  backgroundColor,
   disabled,
   level,
   onPress,
   ...props
 }) {
+  const [hover, setHover] = useState()
+  const wrapperExtraStyles = useMemo(() => {
+    if (!backgroundColor) return {}
+    if (hover) {
+      return { backgroundColor: alpha(backgroundColor, opacity.hover) }
+    } else {
+      return { backgroundColor }
+    }
+  }, [hover, backgroundColor])
+
   const isClickable = typeof onPress === 'function' && !disabled
   let Wrapper = isClickable
     ? TouchableOpacity
@@ -25,11 +39,14 @@ function Div ({
   if (isClickable) {
     extraProps.activeOpacity = config.opacity.active
     extraProps.onPress = onPress
+
+    extraProps.onMouseEnter = () => setHover(true)
+    extraProps.onMouseLeave = () => setHover()
   }
 
   return pug`
     Wrapper.root(
-      style=[style, SHADOWS[level]]
+      style=[style, SHADOWS[level], wrapperExtraStyles]
       styleName=[{
         withShadow: !!level,
         clickable: isClickable
@@ -37,18 +54,20 @@ function Div ({
       ...extraProps
       ...props
     )
+      View.content
       = children
   `
 }
 
 Div.defaultProps = {
-  level: 0,
-  disabled: false
+  disabled: false,
+  level: 0
 }
 
 Div.propTypes = {
-  level: propTypes.oneOf(SHADOWS.map((key, index) => index)),
+  backgroundColor: propTypes.string,
   disabled: propTypes.bool,
+  level: propTypes.oneOf(SHADOWS.map((key, index) => index)),
   onPress: propTypes.func
 }
 
