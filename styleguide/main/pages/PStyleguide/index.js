8ac0bfbd4ff97b509bdb33f2f89542e72d99da6e
Pavel Turchik <pturchik@yandex.ru>
2020-02-10 17:48:19 +0300

    remove redundant

--- styleguide/main/pages/PStyleguide/index.js
+++ styleguide/main/pages/PStyleguide/index.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { observer } from 'startupjs'
 import { Props } from 'components'
-import { TextInput } from 'react-native'
 import * as COMPONENTS from 'ui'
 import {
   useComponentName,
@@ -19,45 +18,14 @@ export default observer(function PStyleguide () {
   const [validateWidth] = useValidateWidth()
   const [darkTheme] = useDarkTheme()
   const COMPONENT = COMPONENTS[componentName]
-  const [value, setValue] = useState()
 
   if (!COMPONENT) {
     return pug`
       COMPONENTS.H1 Component not found
     `
   }
-  const Input = COMPONENTS.TextInput
 
   return pug`
-    COMPONENTS.Row
-      TextInput(value='hello')
-      Input(
-        value=value
-        placeholder='write here'
-        onChangeText=(value) => {
-          console.log(value, 'myyyvalue')
-          setValue(value)
-        }
-      )
-      //- style={marginLeft: -150, zIndex: 100, background: 'transparent'}
-      Input(
-        value=value
-        placeholder='write here'
-        numberOfLines=4
-        onChangeText=(value) => {
-          console.log(value, 'value')
-          setValue(value)
-        }
-      )
-      //- Input(
-      //-   value=value
-      //-   placeholder='write here'
-      //-   resize=true
-      //-   onChangeText=(value) => {
-      //-     console.log(value, 'value')
-      //-     setValue(value)
-      //-   }
-      //- )
     Props.root(
       theme=darkTheme ? 'dark' : undefined
       key=componentName
