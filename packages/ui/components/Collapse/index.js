02906f48a8d41ab3d6779634a708f3df8d6e8687
Pavel Turchik <pturchik@yandex.ru>
2020-02-14 13:15:25 +0300

    [ui] fix collapse

--- packages/ui/components/Collapse/index.js
+++ packages/ui/components/Collapse/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import propTypes from 'prop-types'
 import { observer } from 'startupjs'
 import { View } from 'react-native'
+import Div from './../Div'
 import Span from './../Span'
 import Row from './../Row'
 import Icon from './../Icon'
@@ -10,13 +11,20 @@ import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 import config from './../../config/rootConfig'
 import './index.styl'
 
-function Collapse ({ style, title, open, children }) {
+function Collapse ({ style, title, open, children, onChange }) {
+  const collapsed = !open
+
+  function toggle () {
+    onChange(collapsed)
+  }
+
   return pug`
-    View.root(style=style)
-      Row.title(align='between' vAlign='center')
-        Span.titleText(size='l' numberOfLines=1 bold)= title
-        Icon(icon=faCaretDown color=config.colors.dark)
-      Collapsible(collapsed=!open)
+    Div.root(style=style level=1)
+      Div(onPress=toggle)
+        Row.title(align='between' vAlign='center')
+          Span.titleText(size='l' numberOfLines=1 bold)= title
+          Icon(icon=faCaretDown color=config.colors.dark)
+      Collapsible(collapsed=collapsed)
         View.content= children
   `
 }
