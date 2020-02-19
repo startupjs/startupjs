44e03aa1bf045407d4097244f02a7f78bc89f6e1
tohellandback <ivangitmail@gmail.com>
2020-02-19 16:11:19 +0300

    [ui -> Tag] refactor

--- packages/ui/components/Div/index.js
+++ packages/ui/components/Div/index.js
@@ -1,5 +1,5 @@
 import React, { useMemo, useState } from 'react'
-import { View, TouchableOpacity } from 'react-native'
+import { View, TouchableOpacity, Platform } from 'react-native'
 import propTypes from 'prop-types'
 import { observer, useDidUpdate } from 'startupjs'
 import config from '../../config/rootConfig'
@@ -7,6 +7,7 @@ import './index.styl'
 
 const { hoverStateOpacity, activeStateOpacity } = config.Div
 const SHADOWS = config.shadows
+const isWeb = Platform.OS === 'web'
 
 function Div ({
   style,
@@ -15,8 +16,6 @@ function Div ({
   level,
   interactive,
   activeOpacity,
-  onMouseEnter,
-  onMouseLeave,
   onPress,
   ...props
 }) {
@@ -26,28 +25,29 @@ function Div ({
   const Wrapper = isClickable ? TouchableOpacity : View
 
   const extraProps = useMemo(() => {
-    let props = {}
+    let _props = {}
 
     if (isClickable) {
-      props.activeOpacity = 1
-      props.onPress = onPress
+      _props.activeOpacity = 1
+      _props.onPress = onPress
 
-      if (interactive) {
-        props.activeOpacity = activeOpacity
+      if (interactive && isWeb) {
+        const { onMouseEnter, onMouseLeave } = props
+        _props.activeOpacity = activeOpacity
 
-        props.onMouseEnter = (...args) => {
+        _props.onMouseEnter = (...args) => {
           setHover(true)
           onMouseEnter && onMouseEnter(...args)
         }
 
-        props.onMouseLeave = (...args) => {
+        _props.onMouseLeave = (...args) => {
           setHover()
           onMouseLeave && onMouseLeave(...args)
         }
       }
     }
 
-    return props
+    return _props
   }, [isClickable, interactive])
 
   const extraStyles = useMemo(() => {
@@ -68,8 +68,8 @@ function Div ({
     Wrapper.root(
       style=[style, SHADOWS[level], extraStyles]
       styleName=[{ ['with-shadow']: !!level }]
-      ...extraProps
       ...props
+      ...extraProps
     )
       = children
   `
--- packages/ui/components/Tag/index.js
+++ packages/ui/components/Tag/index.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useMemo, useState } from 'react'
+import { View, Platform } from 'react-native'
 import { observer } from 'startupjs'
+import propTypes from 'prop-types'
 import Div from '../Div'
 import Span from '../Span'
 import Icon from '../Icon'
-import propTypes from 'prop-types'
+import colorToRGBA from '../../config/colorToRGBA'
 import config from '../../config/rootConfig'
 import './index.styl'
 
 const { colors } = config
-
 const ICON_PROPS = {
   size: 'xs'
 }
+const isWeb = Platform.OS === 'web'
 
 function Tag ({
   style,
@@ -26,7 +27,54 @@ function Tag ({
   onPress,
   ...props
 }) {
-  const _backgroundColor = colors[color] || color
+  const [hover, setHover] = useState()
+  const [active, setActive] = useState()
+  const isClickabe = typeof onPress === 'function'
+
+  const extraProps = useMemo(() => {
+    let _props = {}
+    if (isClickabe) {
+      const {
+        onMouseEnter,
+        onMouseLeave,
+        onPressIn,
+        onPressOut
+      } = props
+
+      if (isWeb) {
+        _props.onMouseEnter = (...args) => {
+          setHover(true)
+          onMouseEnter && onMouseEnter(...args)
+        }
+
+        _props.onMouseLeave = (...args) => {
+          setHover()
+          onMouseLeave && onMouseLeave(...args)
+        }
+      }
+
+      _props.onPressIn = (...args) => {
+        setActive(true)
+        onPressIn && onPressIn(...args)
+      }
+
+      _props.onPressOut = (...args) => {
+        setActive()
+        onPressOut && onPressOut()
+      }
+    }
+    return _props
+  }, [isClickabe])
+
+  const _backgroundColor = useMemo(() => {
+    const backgroundColor = colors[color] || color
+
+    if (active) return colorToRGBA(backgroundColor, 0.25)
+    if (hover) return colorToRGBA(backgroundColor, 0.5)
+
+    return backgroundColor
+  }, [hover, active, color])
+
   const _textColor = colors[textColor] || textColor || colors.white
   const _iconsColor = colors[iconsColor] || iconsColor || colors.white
 
@@ -34,10 +82,12 @@ function Tag ({
 
   return pug`
     Div.root(
-      style=style
+      style=[style, { backgroundColor: _backgroundColor}]
       styleName=[color, variant]
-      backgroundColor=_backgroundColor
+      interactive=false
       onPress=onPress
+      ...props
+      ...extraProps
     )
       if icon
         View.leftIconWrapper(styleName=[iconWrapperStyle])
