be2b902be912f7c74972da5d5c27ce76c71c23d7
tohellandback <ivangitmail@gmail.com>
2020-02-19 11:50:40 +0300

    [ui -> Div] refactor

--- packages/ui/components/Div/index.js
+++ packages/ui/components/Div/index.js
@@ -1,93 +1,43 @@
-import React, { useState, useMemo, useEffect } from 'react'
+import React, { useMemo } from 'react'
 import { View, TouchableOpacity } from 'react-native'
 import propTypes from 'prop-types'
 import { observer } from 'startupjs'
 import config from '../../config/rootConfig'
-import { colorToRGBA } from '../../config/helpers'
 import './index.styl'
 
-const { activeStateOpacity, hoverOpacity } = config.Div
-const { colors } = config
+const { activeStateOpacity } = config.Div
 const SHADOWS = config.shadows
 
 function Div ({
   style,
   children,
-  backgroundColor,
   disabled,
   level,
   interactive,
-  onMouseLeave,
-  onMouseEnter,
+  activeOpacity,
   onPress,
   ...props
 }) {
-  // Shadow does not work without backgorund color
-  const _backgroundColor = backgroundColor || (level ? colors.white : null)
-
-  const [hover, setHover] = useState()
-  const [active, setActive] = useState()
-  const wrapperExtraStyles = useMemo(() => {
-    if (!_backgroundColor) return {}
-    if (interactive) {
-      if (active) {
-        return {
-          backgroundColor: colorToRGBA(_backgroundColor, activeStateOpacity)
-        }
-      }
-      if (hover) {
-        return {
-          backgroundColor: colorToRGBA(_backgroundColor, hoverOpacity)
-        }
-      }
-    }
-    return { backgroundColor: _backgroundColor }
-  }, [hover, active, _backgroundColor])
-
   const isClickable = typeof onPress === 'function' && !disabled
-
-  const extraProps = {}
-
-  const isInteractiveWithoutBg = !_backgroundColor && interactive
-
-  if (isClickable) {
-    extraProps.activeOpacity = isInteractiveWithoutBg
-      ? activeStateOpacity
-      : 1
-
-    extraProps.onPress = onPress
-
-    extraProps.onMouseEnter = (...args) => {
-      setHover(true)
-      onMouseEnter && onMouseEnter(...args)
-    }
-    extraProps.onMouseLeave = (...args) => {
-      setHover()
-      onMouseLeave && onMouseLeave(...args)
-    }
-
-    extraProps.onPressIn = () => setActive(true)
-    extraProps.onPressOut = () => setActive()
-  }
 
   const Wrapper = isClickable ? TouchableOpacity : View
 
-  // If component become not clickable, for example received 'disabled' prop while hover or active,
-  // state wouldn't update without this effect
-  useEffect(() => {
-    if (!isClickable) {
-      setHover()
-      setActive()
+  const extraProps = useMemo(() => {
+    if (isClickable) {
+      return { onPress }
     }
+    return {}
   }, [isClickable])
 
   return pug`
     Wrapper.root(
-      style=[style, SHADOWS[level], wrapperExtraStyles]
+      style=[style, SHADOWS[level]]
       styleName=[{
         clickable: isClickable,
-        ['interactive']: isInteractiveWithoutBg
+        ['with-shadow']: !!level,
+        interactive
       }]
+      activeOpacity=interactive ? activeOpacity : 1
       ...extraProps
       ...props
     )
@@ -96,18 +46,19 @@ function Div ({
 }
 
 Div.defaultProps = {
+  activeOpacity: activeStateOpacity,
   disabled: false,
-  level: 0,
-  interactive: true
+  interactive: true,
+  level: 0
 }
 
 Div.propTypes = {
   style: propTypes.oneOfType([propTypes.object, propTypes.array]),
-  children: propTypes.node,
-  backgroundColor: propTypes.string,
+  activeOpacity: propTypes.number,
   disabled: propTypes.bool,
-  level: propTypes.oneOf(SHADOWS.map((key, index) => index)),
   interactive: propTypes.bool,
+  level: propTypes.oneOf(SHADOWS.map((key, index) => index)),
+  children: propTypes.node,
   onPress: propTypes.func
 }
 
