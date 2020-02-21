6be9511112ded4b524a41998c10c815564c4816a
tohellandback <ivangitmail@gmail.com>
2020-02-21 11:42:09 +0300

    [ui -> InputWrapper] add animation on hover

--- packages/ui/components/forms/InputWrapper/index.js
+++ packages/ui/components/forms/InputWrapper/index.js
@@ -6,7 +6,7 @@ import colorToRGBA from '../../../config/colorToRGBA'
 import './index.styl'
 
 const SCALE_TIMING = 280
-const FADE_TIMING = 120
+const FADE_TIMING = 80
 
 function InputWrapper ({
   style,
@@ -18,18 +18,23 @@ function InputWrapper ({
   shape,
   ...props
 }) {
+  // need this variable to controll active state inside wrapper
+  const [_active, setActive] = useState(active)
   const [scaleAnimation] = useState(new Animated.Value(0))
   const [opacityAnimation] = useState(new Animated.Value(1))
 
   const backgroundColor = useMemo(() => {
     if (!hover && !active && !checked) return
-    if (active) return colorToRGBA(color, 0.25)
+    if (_active) return colorToRGBA(color, 0.25)
     if (hover) return colorToRGBA(color, 0.05)
     if (checked) return colorToRGBA(color, 0.25)
-  }, [color, hover, active, checked])
+  }, [color, hover, active, checked, _active])
 
   useDidUpdate(() => {
-    if (active) scaleAnimation.setValue(0)
+    if (active) {
+      setActive(true)
+      scaleAnimation.setValue(0)
+    }
     opacityAnimation.setValue(1)
     Animated.timing(
       scaleAnimation,
@@ -45,25 +50,30 @@ function InputWrapper ({
             toValue: 0,
             duration: FADE_TIMING
           }
-        ).start()
+        ).start(() => {
+          setActive()
+          if (hover) opacityAnimation.setValue(1)
+        })
       }
     })
   }, [active])
 
-  // useDidUpdate(() => {
-  //   if (hover) {
-  //     Animated.timing(
-  //       scaleAnimation,
-  //       {
-  //         toValue: 1,
-  //         duration: SCALE_TIMING
-  //       }
-  //     ).start()
-  //   } else {
-  //     scaleAnimation.setValue(0)
-  //     opacityAnimation.setValue(1)
-  //   }
-  // }, [hover])
+  useDidUpdate(() => {
+    if (hover) {
+      scaleAnimation.setValue(0)
+      opacityAnimation.setValue(1)
+      Animated.timing(
+        scaleAnimation,
+        {
+          toValue: 1,
+          duration: SCALE_TIMING
+        }
+      ).start()
+    } else {
+      scaleAnimation.setValue(0)
+      opacityAnimation.setValue(1)
+    }
+  }, [hover])
 
   // Cleanup effect !!!
 
