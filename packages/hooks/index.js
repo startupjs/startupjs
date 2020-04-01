28cffec2f6ce6c4fd22f1ecab32d5b4a0364262b
tohellandback <ivangitmail@gmail.com>
2020-04-01 15:48:23 +0300

    [hooks, init] add compile scripts

--- packages/hooks/index.js
+++
@@ -1,4 +0,0 @@
-export { default as useDidUpdate } from './lib/useDidUpdate'
-export { default as useForceUpdate } from './lib/useForceUpdate'
-export { default as useOnce } from './lib/useOnce'
-export { default as useSyncEffect } from './lib/useSyncEffect'
---
+++ packages/hooks/src/index.js
@@ -0,0 +1,4 @@
+export { default as useDidUpdate } from './useDidUpdate'
+export { default as useForceUpdate } from './useForceUpdate'
+export { default as useOnce } from './useOnce'
+export { default as useSyncEffect } from './useSyncEffect'
