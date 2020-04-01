4acc8b06ce09785bd84355fa5d4c454d9caf99cc
Pavel Zhukov <cray0000@gmail.com>
2020-04-02 02:01:38 +0300

    [styleguide -> Sandbox] fix showing active page. Write a TODO for properly setting  in react 16.13

--- styleguide/main/Layout/Sidebar/index.js
+++ styleguide/main/Layout/Sidebar/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { observer, useModel } from 'startupjs'
+import { observer, useModel, useLocal } from 'startupjs'
 import { View, ScrollView, Switch } from 'react-native'
 import * as COMPONENTS from 'ui'
 import docs from '@startupjs/ui/docs'
@@ -24,8 +24,25 @@ const {
 } = COMPONENTS
 
 const SidebarContent = observer(() => {
-  const [componentName, setComponentName] = useComponentName()
-  const [docName, setDocName] = useDocName()
+  // TODO: fix this. It doesn't work with the useParams() hooks properly
+  //       because it's located above the current route and because of this
+  //       the params are always undefined.
+  //       And there is a new warning in React 16.13 which fires for the case
+  //       of setting $render.params in the app/Router component:
+  //       https://reactjs.org/blog/2020/02/26/react-v16.13.0.html
+  //       Also see this issue: https://github.com/facebook/react/issues/18178
+  //
+  //       The solution is to somehow set $render not during the actual rendering
+  //       of Router components but separately, before the Router is going to be rendered.
+  //       Probably the best way to do this is to custom handle setting $render
+  //       inside the handler function of emit('url', handler).
+  //
+  //       And when rendering first time from server, $render should be auto populated
+  //       from the server side.
+  const [, setComponentName] = useComponentName()
+  const [componentName] = useLocal('$render.params.componentName')
+  const [, setDocName] = useDocName()
+  const [docName] = useLocal('$render.params.docName')
   const [showGrid, $showGrid] = useShowGrid()
   const [showSizes, $showSizes] = useShowSizes()
   const [validateWidth, $validateWidth] = useValidateWidth()
@@ -61,25 +78,22 @@ const SidebarContent = observer(() => {
   return pug`
     ScrollView
       Menu
-        Collapse(
-          key='docs'
-          open=openedCollapses.docs
-          onChange=toggleCollapse.bind(null, 'docs')
-        )
-          Collapse.Header Documentation
-          Collapse.Content
-            each aDocName in Object.keys(docs)
-              Menu.Item(
-                key=aDocName
-                active=aDocName === docName
-                onPress=() => setDocName(aDocName)
-              )= aDocName
+        Br(half)
+        each aDocName in Object.keys(docs)
+          Menu.Item(
+            key=aDocName
+            active=aDocName === docName
+            onPress=() => setDocName(aDocName)
+          )= aDocName
+        Br(half)
         Collapse(
           key='sandbox'
+          variant='pure'
           open=openedCollapses.sandbox
           onChange=toggleCollapse.bind(null, 'sandbox')
         )
-          Collapse.Header Sandbox
+          Collapse.Header
+            Span.sandbox Sandbox
           Collapse.Content
             each COMPONENT_NAME in getAvailableComponents(Object.keys(COMPONENTS))
               - const COMPONENT = COMPONENTS[COMPONENT_NAME]
