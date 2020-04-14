5cb38203dbd35242dbda7da10756c5107f288f1b
Pavel Zhukov <cray0000@gmail.com>
2020-04-14 17:28:01 +0300

    [bundler] Add ability to use hooks in the styleguide examples

--- packages/bundler/lib/mdxExamples.js
+++ packages/bundler/lib/mdxExamples.js
@@ -1,5 +1,21 @@
-const REGEX = /(```(?:jsx?|html)) +example([\s\S]*?)(```)/g
+const REGEX = /(```jsx) +example([\s\S]*?)(```)/g
 
 module.exports = function mdxExamplesLoader (source) {
-  return source.replace(REGEX, '$1$2$3\n<example>$2</example>')
+  return source.replace(REGEX, replacer)
+}
+
+function replacer (match, p1, p2, p3) {
+  const code = `${p1}${p2}${p3}\n`
+  p2 = p2.trim()
+  if (/^</.test(p2)) p2 = 'return (<React.Fragment>' + p2 + '</React.Fragment>)'
+
+  return (
+    code + `<example>
+      <React.Fragment>
+        {React.createElement(require('startupjs').observer(function Example () {
+          ${p2}
+        }))}
+      </React.Fragment>
+    </example>`
+  )
 }
