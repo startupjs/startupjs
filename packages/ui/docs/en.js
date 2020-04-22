fe48b79df3fb1db027d66ec5f04348ab6872b240
Veda <pal.veda.deepta@gmail.com>
2020-04-22 21:29:52 +0530

    feat[docs -> Breadcrumbs]: implemented docs

--- packages/ui/docs/en.js
+++ packages/ui/docs/en.js
@@ -1,7 +1,9 @@
 import Button from '../components/Button/Button.en.mdx'
 import Tag from '../components/Tag/Tag.en.mdx'
+import Breadcrumbs from '../components/Breadcrumbs/Breadcrumbs.en.mdx'
 
 export default {
   Button,
+  Breadcrumbs,
   Tag
 }
