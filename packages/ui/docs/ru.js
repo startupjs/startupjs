3a9bafffd5f613a87feb12d408f4bade1c6f6e1c
simonz_git <xeywar0@yandex.ru>
2020-04-23 01:05:47 +0300

    [ui] add Dropdown

--- packages/ui/docs/ru.js
+++ packages/ui/docs/ru.js
@@ -1,5 +1,7 @@
 import Button from '../components/Button/Button.ru.mdx'
+import Dropdown from '../components/popups/Dropdown/Dropdown.ru.mdx'
 
 export default {
-  Button
+  Button,
+  Dropdown
 }
--- packages/ui/hooks/index.js
+++ packages/ui/hooks/index.js
@@ -1,2 +1,4 @@
 export { default as useLayout } from './useLayout'
 export { default as useBindingProps } from './useBindingProps'
+export { default as useRefCallback } from './useRefCallback'
+export { default as useDrawerDismiss } from './useDrawerDismiss'
