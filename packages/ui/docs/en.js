47b81cb71426d754872c2e8a801389ade1297463
Veda <pal.veda.deepta@gmail.com>
2020-04-23 22:13:35 +0530

    feat[docs -> Collapse]: add Collapse docs

--- packages/ui/docs/en.js
+++ packages/ui/docs/en.js
@@ -1,7 +1,9 @@
 import Button from '../components/Button/Button.en.mdx'
+import Collapse from '../components/Collapse/Collapse.en.mdx'
 import Tag from '../components/Tag/Tag.en.mdx'
 
 export default {
   Button,
+  Collapse,
   Tag
 }
