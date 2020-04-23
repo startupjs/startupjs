2629afd9b70e4da7452ce2abba58bdbd35973d7f
Veda <pal.veda.deepta@gmail.com>
2020-04-23 18:00:51 +0530

    feat[docs -> Avatar]: docs for Avatar

--- packages/ui/docs/en.js
+++ packages/ui/docs/en.js
@@ -1,7 +1,9 @@
+import Avatar from '../components/Avatar/Avatar.en.mdx'
 import Button from '../components/Button/Button.en.mdx'
 import Tag from '../components/Tag/Tag.en.mdx'
 
 export default {
+  Avatar,
   Button,
   Tag
 }
