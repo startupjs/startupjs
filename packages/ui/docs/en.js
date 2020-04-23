1494a39ad8ee8288a6c3494b45b68c1e92168a49
Veda <pal.veda.deepta@gmail.com>
2020-04-23 19:11:30 +0530

    feat[docs->Alert]: Alert docs

--- packages/ui/docs/en.js
+++ packages/ui/docs/en.js
@@ -1,7 +1,9 @@
+import Alert from '../components/Alert/Alert.en.mdx'
 import Button from '../components/Button/Button.en.mdx'
 import Tag from '../components/Tag/Tag.en.mdx'
 
 export default {
+  Alert,
   Button,
   Tag
 }
