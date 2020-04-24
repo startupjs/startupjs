8eb8b90648c8b6d6e06d60f479cf2779eb488e81
Pavel Turchik <pturchik@yandex.ru>
2020-04-24 20:14:52 +0300

    docs(ui Breadcrumbs) add ru doc

--- packages/ui/docs/ru.js
+++ packages/ui/docs/ru.js
@@ -1,11 +1,13 @@
 import Alert from '../components/Alert/Alert.ru.mdx'
 import Avatar from '../components/Avatar/Avatar.ru.mdx'
+import Breadcrumbs from '../components/Breadcrumbs/Breadcrumbs.ru.mdx'
 import Button from '../components/Button/Button.ru.mdx'
 import Tag from '../components/Tag/Tag.ru.mdx'
 
 export default {
   Alert,
   Avatar,
+  Breadcrumbs,
   Button,
   Tag
 }
