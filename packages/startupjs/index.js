4c051bc0bda47fe9d5526c33e16db59e5351ef98
Pavel Zhukov <cray0000@gmail.com>
2020-04-27 16:26:09 +0300

    feat(isomorphic-helpers): add new package for helpers which are isomorphic (can be executed both on the client and on the server)

--- packages/startupjs/index.js
+++ packages/startupjs/index.js
@@ -1,4 +1,7 @@
 module.exports = Object.assign({},
   require('@startupjs/react-sharedb/src'),
-  require('@startupjs/hooks/src')
+  require('@startupjs/hooks/src'),
+  // HINT: `isomorphic` means that the code can be executed both
+  //        on the server and on the client
+  require('@startupjs/isomorphic-helpers')
 )
