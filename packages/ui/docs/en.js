5f243303fb2eb221291ee5a01d35dacabca14dde
Veda <pal.veda.deepta@gmail.com>
2020-05-04 19:04:27 +0530

    docs(ui): modal docs (#111)

--- packages/ui/docs/en.js
+++ packages/ui/docs/en.js
@@ -13,6 +13,7 @@ import Layout from '../components/Layout/Layout.en.mdx'
 import Link from '../components/Link/Link.en.mdx'
 import LineSeparator from '../components/LineSeparator/LineSeparator.en.mdx'
 import Loader from '../components/Loader/Loader.en.mdx'
+import Modal from '../components/Modal/Modal.en.mdx'
 import Menu from '../components/Menu/Menu.en.mdx'
 import Progress from '../components/Progress/Progress.en.mdx'
 import Radio from '../components/forms/Radio/Radio.en.mdx'
@@ -38,6 +39,7 @@ export default {
   Link,
   LineSeparator,
   Loader,
+  Modal,
   Menu,
   Progress,
   Radio,
