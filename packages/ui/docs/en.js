e287efc7973903ccdcb1c8925a63c2faaa65f3bf
Veda <pal.veda.deepta@gmail.com>
2020-05-04 18:19:17 +0530

    docs(ui): Row docs (#121)

--- packages/ui/docs/en.js
+++ packages/ui/docs/en.js
@@ -14,6 +14,7 @@ import Link from '../components/Link/Link.en.mdx'
 import Loader from '../components/Loader/Loader.en.mdx'
 import Progress from '../components/Progress/Progress.en.mdx'
 import Rating from '../components/Rating/Rating.en.mdx'
+import Row from '../components/Row/Row.en.mdx'
 import Tag from '../components/Tag/Tag.en.mdx'
 
 export default {
@@ -33,5 +34,6 @@ export default {
   Loader,
   Progress,
   Rating,
+  Row,
   Tag
 }
