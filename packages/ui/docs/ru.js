41a1a8816ec4b12f474560ebad6a042b890ba3b7
Pavel Turchik <pturchik@yandex.ru>
2020-05-06 16:21:55 +0300

    docs(ui): add br docs

--- packages/ui/docs/ru.js
+++ packages/ui/docs/ru.js
@@ -1,5 +1,6 @@
 import Alert from '../components/Alert/Alert.ru.mdx'
 import Avatar from '../components/Avatar/Avatar.ru.mdx'
+import Br from '../components/Br/Br.ru.mdx'
 import Breadcrumbs from '../components/Breadcrumbs/Breadcrumbs.ru.mdx'
 import Button from '../components/Button/Button.ru.mdx'
 import Card from '../components/Card/Card.ru.mdx'
@@ -17,6 +18,7 @@ import Tag from '../components/Tag/Tag.ru.mdx'
 export default {
   Alert,
   Avatar,
+  Br,
   Breadcrumbs,
   Button,
   Card,
