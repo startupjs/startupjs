a8d4a94ea206dbf29f7b67c19a2fccc972edc7cb
Pavel Turchik <pturchik@yandex.ru>
2020-05-06 17:42:55 +0300

    docs(ui): add user component docs

--- packages/ui/docs/ru.js
+++ packages/ui/docs/ru.js
@@ -14,6 +14,7 @@ import Loader from '../components/Loader/Loader.ru.mdx'
 import Progress from '../components/Progress/Progress.ru.mdx'
 import Rating from '../components/Rating/Rating.ru.mdx'
 import Tag from '../components/Tag/Tag.ru.mdx'
+import User from '../components/User/User.ru.mdx'
 
 export default {
   Alert,
@@ -31,5 +32,6 @@ export default {
   Loader,
   Progress,
   Rating,
-  Tag
+  Tag,
+  User
 }
