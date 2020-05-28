a240fc0d016b75b3d59962ba81163e976bd93dbf
vladagurets <aguretsvlad@gmail.com>
2020-05-28 17:29:30 +0300

    Update templates

--- packages/startupjs/templates/routing/Root/App.js
+++ packages/startupjs/templates/routing/Root/App.js
@@ -1,12 +1,15 @@
 import { BASE_URL } from '@env'
 import init from 'startupjs/init'
 import orm from '../model'
-import React, { useMemo } from 'react'
+import React from 'react'
 import App from 'startupjs/app'
-import { observer, useModel, useSession, useDoc } from 'startupjs'
+import { observer, model } from 'startupjs'
+import { Platform } from 'react-native'
 
 // Frontend micro-services
 import * as main from '../main'
+
+if (Platform.OS === 'web') window.model = model
 
 // Init startupjs connection to server and the ORM.
 // baseUrl option is required for the native to work - it's used
@@ -14,33 +17,10 @@ import * as main from '../main'
 // Initialization must start before doing any subscribes to data.
 init({ baseUrl: BASE_URL, orm })
 
-// Global initialization logic
-function useGlobalInit (session) {
-  let $session = useModel('_session')
-
-  // Initialize _session on mobile
-  useMemo(() => session && $session.setEach(session), [])
-
-  let [userId] = useSession('userId')
-  let [user, $user] = useDoc('users', userId)
-
-  // Create an anonymous user data if it doesn't exist.
-  // You would want to remove this logic after adding real authorization.
-  if (!user) {
-    console.warn('Anonymous user detected. Creating DUMMY user data...')
-    throw $user.create()
-  }
-
-  useMemo(() => {
-    // reference self to '_session.user' for easier access
-    $session.ref('user', $user)
-  }, [])
-}
-
-export default observer(({ session }) => {
-  useGlobalInit(session)
-
+export default observer(() => {
   return pug`
-    App(apps={main})
+    App(
+      apps={main}
+    )
   `
 })
