a820880d60390cab3982742b3ce188dd50566397
Pavel Zhukov <cray0000@gmail.com>
2020-07-03 00:42:26 +0300

    fix(template): Don't destructure json

--- packages/startupjs/templates/ui/main/Layout/index.js
+++ packages/startupjs/templates/ui/main/Layout/index.js
@@ -3,7 +3,9 @@ import { observer, emit, useValue, useLocal } from 'startupjs'
 import './index.styl'
 import { Row, Div, Layout, SmartSidebar, Menu, Button, H1, Span } from '@startupjs/ui'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-import { displayName } from '../../app.json'
+import APP from '../../app.json'
+
+const { displayName } = APP
 
 const APP_NAME = displayName.charAt(0).toUpperCase() + displayName.slice(1)
 
