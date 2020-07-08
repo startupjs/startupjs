497f275552c4bbb744ebc2318a8e76fc57f2fe78
Vladimir <vkutsenko@deloitte.com>
2020-07-08 16:16:29 +0300

    fix(SmartSidebar): add sidebar option

--- packages/ui/components/SmartSidebar/index.js
+++ packages/ui/components/SmartSidebar/index.js
@@ -25,6 +25,7 @@ function SmartSidebar ({
   backgroundColor,
   children,
   renderContent,
+  defaultOpen,
   ...props
 }) {
   if (path) {
@@ -70,6 +71,7 @@ function SmartSidebar ({
         forceClosed=forceClosed
         backgroundColor=backgroundColor
         renderContent=renderContent
+        defaultOpen=defaultOpen
       )= children
     else
       DrawerSidebar(
