7d41fda60df34cd5f25bda0e832ed70e159b2e13
Pavel Turchik <pturchik@gmail.com>
2020-07-22 18:22:36 +0300

    fix(mdx): handle case when there is no language

--- packages/mdx/client/Code/index.js
+++ packages/mdx/client/Code/index.js
@@ -25,9 +25,11 @@ export default memo(function Code ({
   ...props
 }) {
   if (typeof children !== 'string') throw Error('[Code] Code must be a string')
-  const code = useMemo(() => {
-    return renderer(refractor.highlight(children, language), textStyle)
-  }, [children])
+  const code = language
+    ? useMemo(() => {
+      return renderer(refractor.highlight(children, language), textStyle)
+    }, [children])
+    : children
   return pug`
     Span.root(...props style=[textStyle, style])= code
   `
