536f928083d171f2776cebe672b9e741f603440b
Pavel Zhukov <cray0000@gmail.com>
2020-09-01 21:13:19 +0300

    fix(bundler): automatically escape exported stylus values

--- packages/bundler/lib/cssToReactNativeLoader.js
+++ packages/bundler/lib/cssToReactNativeLoader.js
@@ -1,9 +1,11 @@
 // ref: https://github.com/kristerkari/react-native-css-transformer
 const css2rn = require('@startupjs/css-to-react-native-transform').default
 
+const EXPORT_REGEX = /:export\s*\{/
+
 // TODO: Fix parsing rgba() colors from stylus. For some reason they are (null)
 module.exports = function cssToReactNative (source) {
-  // TODO: Preprocess :export properties to put their values in quotes
+  source = escapeExport(source)
   const cssObject = css2rn(source, { parseMediaQueries: true, parsePartSelectors: true })
   for (const key in cssObject.__exportProps || {}) {
     cssObject[key] = parseStylValue(cssObject.__exportProps[key])
@@ -30,4 +32,43 @@ function parseStylValue (value) {
   } else {
     return value
   }
+}
+
+// Process :export properties by wrapping their values in quotes
+function escapeExport (source) {
+  const match = source.match(EXPORT_REGEX)
+  if (!match) return source
+
+  // 1. find closing bracket of :export { ... }
+  const matchIndex = match.index
+  const matchStr = match[0]
+  const matchLength = matchStr.length
+  const start = matchIndex + matchLength
+  let openBr = 1 // Count opened brackets, we start from one already opened
+  let end
+
+  for (let i = start; i < source.length; i++) {
+    if (source.charAt(i) === '}') {
+      --openBr
+    } else if (source.charAt(i) === '{') {
+      ++openBr
+    }
+
+    if (openBr <= 0) {
+      end = i
+      break
+    }
+  }
+  if (!end) return source
+
+  // 2. escape all exported values
+  const properties = source
+    .slice(start, end)
+    .split(';')
+    .map(line => line.replace(/(:\s+)([^'"].*[^'"])$/, '$1\'$2\''))
+    .join(';')
+
+  source = source.slice(0, start) + properties + source.slice(end)
+
+  return source
 }
