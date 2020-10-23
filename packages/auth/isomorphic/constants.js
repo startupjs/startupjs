0fb465c1b5e3a813dc4c01100909ac4045812cd8
aguretsvlad <aguretsvlad@gmail.com>
2020-10-23 18:17:29 +0300

    refactor(auth): Rm unused code

--- packages/auth/isomorphic/constants.js
+++ packages/auth/isomorphic/constants.js
@@ -1,3 +1,2 @@
 export const LOGOUT_URL = '/auth/logout'
 export const SIGN_IN_URL = '/auth/sign-in'
-export const AUTHENTICATED_URL = '/authenticated'
--- packages/auth/server/initDefaultRoutes.js
+++ packages/auth/server/initDefaultRoutes.js
@@ -1,4 +1,4 @@
-import { LOGOUT_URL, AUTHENTICATED_URL } from '../isomorphic/constants'
+import { LOGOUT_URL } from '../isomorphic/constants'
 
 export default function initDefaultRoutes (router) {
   router.get(LOGOUT_URL, function (req, res) {
@@ -6,12 +6,5 @@ export default function initDefaultRoutes (router) {
     delete req.session.userId
     req.logout()
     res.redirect('/')
-  })
-  router.get(AUTHENTICATED_URL, function (req, res) {
-    res.send(`
-      <p>Authorization successful!</p>
-      <p>You will be redirected back in just a second.</p>
-      <script>setTimeout(function(){window.location.href = '/'}, 100)</script>
-    `)
   })
 }
