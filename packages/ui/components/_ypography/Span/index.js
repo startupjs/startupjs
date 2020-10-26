c99347ddf200f950d9b4a7bacf463df19054d8a7
lupenq <skinilya@gmail.com>
2020-10-26 16:39:14 +0300

    fix moduleMap

--- packages/ui/components/_ypography/Span/index.js
+++ packages/ui/components/_ypography/Span/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { observer } from 'startupjs'
-import PropTypes from 'prop-types'
 import { Text } from 'react-native'
+import PropTypes from 'prop-types'
 import themed from '../../../theming/themed'
 import './index.styl'
 
