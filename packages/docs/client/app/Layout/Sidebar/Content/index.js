b337acaafa15c841f00c716df428eb53ad844457
Pavel Turchik <pturchik@gmail.com>
2020-11-10 17:37:02 +0300

    feature(docs): add anchors for headers

--- packages/docs/client/app/Layout/Sidebar/Content/index.js
+++ packages/docs/client/app/Layout/Sidebar/Content/index.js
@@ -1,13 +1,13 @@
-import { BASE_URL } from '@env'
 import React from 'react'
-import { observer, $root, emit } from 'startupjs'
-import { ScrollView, Image } from 'react-native'
-import Options from './Options'
-import './index.styl'
+import { Image } from 'react-native'
+import { observer } from 'startupjs'
 import { Div, Row, Select } from '@startupjs/ui'
+import { BASE_URL } from '@env'
 import { useDocsContext } from '../../../../../docsContext'
-import Docs from './Docs'
 import { useLang } from '../../../../clientHelpers'
+import Options from './Options'
+import Docs from './Docs'
+import './index.styl'
 
 const LANGUAGES = [
   { value: 'en', label: 'English' },
@@ -18,28 +18,19 @@ export default observer(function Content ({
   style
 }) {
   const [lang, setLang] = useLang()
-  if (!lang) return null
   // TODO: Change logo image to base64 and pass it through context
   const baseUrl = BASE_URL
   const docs = useDocsContext()
 
-  function switchLanguage (language) {
-    const url = $root.get('$render.url')
-    if (lang === language) return
-    setLang(language)
-    emit('url', url.replace(`/${lang}`, `/${language}`))
-  }
-
   return pug`
     Div.root
-      ScrollView.main
-        Image.logo(source={ uri: baseUrl + '/img/docs.png' })
-        Docs(docs=docs lang=lang)
+      Image.logo(source={ uri: baseUrl + '/img/docs.png' })
+      Docs(docs=docs lang=lang)
       Row.footer
         Select.lang(
           options=LANGUAGES
           value=lang
-          onChange=switchLanguage
+          onChange=setLang
           showEmptyValue=false
         )
         Options.options
