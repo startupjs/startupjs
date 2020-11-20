386b305c899f87d0375f84b49a00bd3ff0099650
ssstelsss <oqjwcqdncqbm@mail.ru>
2020-11-20 16:11:48 +0300

    feedBack

--- packages/app/client/components/AccessDeny/index.js
+++ packages/app/client/components/AccessDeny/index.js
@@ -1,25 +1,37 @@
 import React from 'react'
-import { Text } from 'react-native'
-import { observer, useSession } from 'startupjs'
+import { observer, useSession, emit } from 'startupjs'
+import { Card, H3, H5, Span, Button } from '@startupjs/ui'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import Layout from './../Layout'
 import mailTo from '../Blocked/mailTo'
 import './index.styl'
 
-export default observer(function Blocked () {
+export default observer(function AccessDeny () {
   const [{ supportEmail } = {}] = useSession('criticalVersion.meta')
+  const [, $accessError] = useSession('_accessError')
 
-  const description = pug`
-    Text If you think it's a mistake, please contact support
+  function goTo (path) {
+    $accessError.set('')
+    emit('url', path)
+  }
+
+  const Description = pug`
+    H5 If you think it's a mistake, please contact support
     if supportEmail
-      Text &nbsp;at&nbsp;
-      Text.email(onPress=() => mailTo(supportEmail))= supportEmail
-      Text .
+      Span &nbsp;at&nbsp;
+      Span.email(onPress=() => mailTo(supportEmail))= supportEmail
+      Span .
   `
 
   return pug`
     Layout(
-      title='Sorry. Access deny!'
-      description=description
+      center
     )
+      Card.card
+        H3.info 403: Permission denied
+        =Description
+
+        Button.backButton(icon=faArrowLeft onPress=()=>goTo('/')) Go back
+
   `
 })
