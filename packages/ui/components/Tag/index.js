a1023412f5cdfd117e05a9598a7f31d33e435a39
lupenq <skinilya@gmail.com>
2020-11-23 20:43:05 +0300

    rev

--- packages/ui/components/Tag/index.js
+++ packages/ui/components/Tag/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { observer } from 'startupjs'
+import PropTypes from 'prop-types'
 import Button from '../Button'
 
 function Tag (props) {
@@ -17,6 +18,11 @@ Tag.defaultProps = {
   color: 'primary',
   variant: 'flat',
   shape: 'circle'
+}
+
+Tag.propTypes = {
+  variant: PropTypes.oneOf(['flat', 'outlined']),
+  shape: PropTypes.oneOf(['circle', 'rounded'])
 }
 
 export default observer(Tag)
