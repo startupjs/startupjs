dc98ef1ca6f396b44e4a936997111d1990143e82
lupenq <skinilya@gmail.com>
2020-11-23 20:38:22 +0300

    fix(Tag): del proptypes

--- packages/ui/components/Tag/index.js
+++ packages/ui/components/Tag/index.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import { observer } from 'startupjs'
-import PropTypes from 'prop-types'
 import Button from '../Button'
 
-function Tag ({ size, ...props }) {
+function Tag (props) {
   return pug`
     //- button is clickable by default, override this behavior
     Button(
-      onPress=null
-      size=size
       ...props
+      size='xs'
     )
   `
 }
@@ -18,15 +16,7 @@ Tag.defaultProps = {
   ...Button.defaultProps,
   color: 'primary',
   variant: 'flat',
-  shape: 'circle',
-  size: 'xs'
-}
-
-Tag.propTypes = {
-  ...Button.propTypes,
-  variant: PropTypes.oneOf(['flat', 'outlined']),
-  shape: PropTypes.oneOf(['circle', 'rounded']),
-  size: PropTypes.oneOf(['xs', 's', 'm', 'l', 'xl', 'xxl'])
+  shape: 'circle'
 }
 
 export default observer(Tag)
