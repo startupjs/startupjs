a4ea48870b72c1f528f7606741bdc3402904165a
Oleksandr Pishchuk <ovpischuk@gmail.com>
2020-11-23 13:59:44 +0200

    changed library for Picker and patched

--- packages/ui/components/forms/Select/Wrapper/index.android.js
+++ packages/ui/components/forms/Select/Wrapper/index.android.js
@@ -1,7 +1,6 @@
 // ref: https://github.com/lawnstarter/react-native-picker-select/blob/master/src/index.js
 import React from 'react'
-// TODO: Change to @react-native-picker/picker when the issue is closed https://github.com/react-native-picker/picker/issues/175
-import { Picker } from 'react-native'
+import { Picker } from '@react-native-picker/picker'
 import { observer } from 'startupjs'
 import {
   stringifyValue,
@@ -25,22 +24,6 @@ export default observer(function SelectWrapper ({
     if (onChange) onChange(parseValue(value))
   }
 
-  const items = options.map((item, index) => pug`
-    Picker.Item(
-      key=index
-      value=stringifyValue(item)
-      label=getLabel(item)
-    )
-  `)
-
-  showEmptyValue && items.unshift(pug`
-    Picker.Item(
-      key=-1
-      value=stringifyValue(NULL_OPTION)
-      label=getLabel(NULL_OPTION)
-    )
-  `)
-
   return pug`
     Div.root(style=style)
       = children
@@ -48,6 +31,18 @@ export default observer(function SelectWrapper ({
         Picker.overlay(
           selectedValue=stringifyValue(value)
           onValueChange=onValueChange
-        )= items
+        )
+          if showEmptyValue
+            Picker.Item(
+              key=-1
+              value=stringifyValue(NULL_OPTION)
+              label=getLabel(NULL_OPTION)
+            )
+          each item, index in options
+            Picker.Item(
+              key=index
+              value=stringifyValue(item)
+              label=getLabel(item)
+            )
   `
 })
