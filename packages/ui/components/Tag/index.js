f1bdc0381defaa7e0eaa0fffa2c19a0874b97cbf
lupenq <skinilya@gmail.com>
2020-11-23 20:50:42 +0300

    feedback

--- packages/ui/components/Tag/index.js
+++ packages/ui/components/Tag/index.js
@@ -3,11 +3,13 @@ import { observer } from 'startupjs'
 import PropTypes from 'prop-types'
 import Button from '../Button'
 
+const size = 'xs'
+
 function Tag (props) {
   return pug`
     Button(
       ...props
-      size='xs'
+      size=size
     )
   `
 }
@@ -16,10 +18,13 @@ Tag.defaultProps = {
   ...Button.defaultProps,
   color: 'primary',
   variant: 'flat',
-  shape: 'circle'
+  shape: 'circle',
+  size: 'xs'
 }
 
 Tag.propTypes = {
+  ...Button.propTypes,
+  size: PropTypes.oneOf(['xs']),
   variant: PropTypes.oneOf(['flat', 'outlined']),
   shape: PropTypes.oneOf(['circle', 'rounded'])
 }
