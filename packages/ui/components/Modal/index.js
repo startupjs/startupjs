601107b531a306b3da874c85a7fb250ca72edf48
lupenq <skinilya@gmail.com>
2020-11-30 10:47:15 +0300

    ref modal web

--- packages/ui/components/Modal/index.js
+++ packages/ui/components/Modal/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import { View, Modal as RNModal, Platform } from 'react-native'
-import ReactDOM from 'react-dom'
+import { View, Modal as RNModal } from 'react-native'
 import { observer } from 'startupjs'
 import PropTypes from 'prop-types'
 import Layout from './layout'
 import ModalHeader from './ModalHeader'
 import ModalContent from './ModalContent'
 import ModalActions from './ModalActions'
-
-const ROOT_CONTAINER_ID = 'app'
 
 function Modal ({
   style,
@@ -24,81 +21,40 @@ function Modal ({
 }) {
   if (!visible) return null
 
-  if (props.variant === 'pure') {
-    if (Platform.OS === 'web') {
-      return ReactDOM.createPortal(
-        pug`
-          RNModal(
-            style=style
-            visible
-            transparent
-            animationType=animationType
-            onRequestClose=onRequestClose
-            onOrientationChange=onOrientationChange
-            supportedOrientations=supportedOrientations
-            onDismiss=onDismiss
-            onShow=onShow
-            statusBarTranslucent=statusBarTranslucent
-          )= props.children
-        `,
-        document.getElementById(ROOT_CONTAINER_ID)
-      )
-    } else {
-      return pug`
-        RNModal(
-          style=style
-          visible
-          transparent
-          animationType=animationType
-          onRequestClose=onRequestClose
-          onOrientationChange=onOrientationChange
-          supportedOrientations=supportedOrientations
-          onDismiss=onDismiss
-          onShow=onShow
-          statusBarTranslucent=statusBarTranslucent
-        )= props.children
-      `
-    }
-  } else {
-    if (Platform.OS === 'web') {
-      return ReactDOM.createPortal(
-        pug`
-          RNModal(
-            style=style
-            visible
-            transparent
-            animationType=animationType
-            onRequestClose=onRequestClose
-            onDismiss=onDismiss
-            onShow=onShow
-          )
-            Layout(
-              modalStyle=style
-              ...props
-            )
-        `,
-        document.getElementById(ROOT_CONTAINER_ID)
-      )
-    } else {
-      return pug`
-        RNModal(
-          visible
-          transparent
-          animationType=animationType
-          onRequestClose=onRequestClose
-          onOrientationChange=onOrientationChange
-          supportedOrientations=supportedOrientations
-          onDismiss=onDismiss
-          onShow=onShow
-          statusBarTranslucent=statusBarTranslucent
-        )
-          Layout(
-            modalStyle=style
-            ...props
-          )
-      `
-    }
+  if (props.variant === 'custom') {
+    return pug`
+      RNModal(
+        style=style
+        visible
+        transparent
+        animationType=animationType
+        onRequestClose=onRequestClose
+        onOrientationChange=onOrientationChange
+        supportedOrientations=supportedOrientations
+        onDismiss=onDismiss
+        onShow=onShow
+        statusBarTranslucent=statusBarTranslucent
+      )= props.children
+    `
   }
+
+  return pug`
+    RNModal(
+      visible
+      transparent
+      animationType=animationType
+      onRequestClose=onRequestClose
+      onOrientationChange=onOrientationChange
+      supportedOrientations=supportedOrientations
+      onDismiss=onDismiss
+      onShow=onShow
+      statusBarTranslucent=statusBarTranslucent
+    )
+      Layout(
+        modalStyle=style
+        ...props
+      )
+  `
 }
 
 Modal.defaultProps = {
@@ -115,7 +71,7 @@ Modal.defaultProps = {
 Modal.propTypes = {
   style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
   children: PropTypes.node,
-  variant: PropTypes.oneOf(['window', 'fullscreen', 'pure']),
+  variant: PropTypes.oneOf(['window', 'fullscreen', 'custom']),
   visible: PropTypes.bool,
   title: PropTypes.string,
   dismissLabel: ModalActions.propTypes.dismissLabel,
