d54c60335badab6c3b20b2bee90602e907bb1d96
Pavel Zhukov <cray0000@gmail.com>
2020-11-30 20:10:08 +0200

    fix: minor

--- packages/startupjs/templates/ui/main/pages/PAbout/index.js
+++ packages/startupjs/templates/ui/main/pages/PAbout/index.js
@@ -8,6 +8,6 @@ export default observer(function PAbout () {
   return pug`
     ScrollView.root
       Content
-        Text.text Built on StartupJS. Local Install
+        Text.text Built on StartupJS
   `
 })
