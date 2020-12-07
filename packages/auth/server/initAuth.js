759253f005b95489dae4bb4791885a697008ca92
aguretsvlad <aguretsvlad@gmail.com>
2020-12-07 13:35:28 +0200

    feat(auth): Add signInPageUrl opt

--- packages/auth/server/initAuth.js
+++ packages/auth/server/initAuth.js
@@ -9,7 +9,7 @@ import {
 } from './helpers'
 import initDefaultRoutes from './initDefaultRoutes'
 import { passportMiddleware } from './middlewares'
-import { DEFAUL_SUCCESS_REDIRECT_URL } from '../isomorphic'
+import { DEFAUL_SUCCESS_REDIRECT_URL, SIGN_IN_URL } from '../isomorphic'
 
 const router = express.Router()
 
@@ -30,6 +30,7 @@ function validateConfigs ({ strategies }) {
 export default function (ee, _config) {
   const config = {}
   Object.assign(config, {
+    signInPageUrl: SIGN_IN_URL,
     parseUserCreationData,
     onBeforeLogoutHook,
     onBeforeLoginHook,
