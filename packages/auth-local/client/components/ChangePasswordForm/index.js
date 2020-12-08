c4271389f5774ebee4323a8236c88aad9877b1d4
aguretsvlad <aguretsvlad@gmail.com>
2020-12-08 16:25:44 +0200

    feat(auth): Add ChangePasswordForm

--- packages/auth-local/client/components/ChangePasswordForm/index.js
+++ packages/auth-local/client/components/ChangePasswordForm/index.js
@@ -1,25 +1,28 @@
 import React from 'react'
-import { observer, useQueryDoc, useValue, useDoc } from 'startupjs'
+import { observer, useValue } from 'startupjs'
 import { useAuthHelper } from '@startupjs/auth-local'
 import { Span, Div, TextInput, Button } from '@startupjs/ui'
 import _get from 'lodash/get'
 import './index.styl'
 
-export default observer(function ResetPasswordForm ({ secret, onSuccess }) {
+export default observer(function ChangePasswordForm ({ onSuccess }) {
   const authHelper = useAuthHelper()
 
   const [form, $form] = useValue({})
   const [error, $error] = useValue()
   const [feedback, $feedback] = useValue()
-  const [auth] = useQueryDoc('auths', { 'providers.local.passwordReset.secret': secret })
-  const [user] = useDoc('users', _get(auth, 'id') || '_DUMMY_')
 
   function _onSuccess () {
     $feedback.set('Your password has been changed successfully.')
-    onSuccess && onSuccess(user.id)
+    onSuccess && onSuccess()
   }
 
   async function save () {
+    if (!form.oldPassword) {
+      $error.set('Please fill old password')
+      return
+    }
+
     if (!form.password || form.password.length < 8) {
       $error.set('Your password must be between 8 - 32 characters long')
       return
@@ -36,20 +39,11 @@ export default observer(function ResetPasswordForm ({ secret, onSuccess }) {
     }
 
     try {
-      authHelper.resetPassword({
-        secret,
-        ...form
-      })
+      await authHelper.changePassword(form)
+
       _onSuccess()
     } catch (error) {
       const errorMsg = _get(error, 'response.data.message')
-      if (errorMsg === 'reset password expired' && user) {
-        $error.set('Link is expired.')
-        return
-      } else if (errorMsg === 'No user found by secret') {
-        $error.set('Link is invalid.')
-        return
-      }
       $error.set(errorMsg)
     }
   }
@@ -63,17 +57,25 @@ export default observer(function ResetPasswordForm ({ secret, onSuccess }) {
     Div.root
       Div.content
         if !feedback
-          Span.header-text Enter your new password
+          Span.header-text Update password
           TextInput.input(
+            label='Enter your old password'
+            value=form.oldPassword
+            onChange=onInputChange('oldPassword')
+            placeholder='Old password'
+            secureTextEntry
+          )
+          TextInput.input(
+            label='Enter your new password'
             value=form.password
             onChange=onInputChange('password')
-            placeholder='Password'
+            placeholder='New Password'
             secureTextEntry
           )
           TextInput.input(
             value=form.confirm
             onChange=onInputChange('confirm')
-            placeholder='Confirm'
+            placeholder='Confirm new password'
             secureTextEntry
           )
           Div.error
