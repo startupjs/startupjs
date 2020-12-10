0c6320e7f44740847c36c02372ced614e5eac162
ssstelsss <oqjwcqdncqbm@mail.ru>
2020-12-10 18:42:39 +0300

    minor

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -123,8 +123,8 @@ module.exports = async options => {
       const { aggregations } = global.STARTUP_JS_ORM[path].OrmEntity
       if (aggregations) {
         for (let aggregationKey in aggregations) {
-          const globalCollectionName = path.replace(/\.\*$/u, '')
-          backend.addAggregate(globalCollectionName, aggregationKey, (queryParams, shareRequest) => {
+          const collection = path.replace(/\.\*$/u, '')
+          backend.addAggregate(collection, aggregationKey, (queryParams, shareRequest) => {
             const session = shareRequest.agent.connectSession
             const userId = session.userId
             const model = global.__clients[userId].model
