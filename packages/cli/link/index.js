b18fa575c61e48d9edaa8b9428b6871d9ceef8fd
Pavel Turchik <pturchik@gmail.com>
2020-12-29 00:18:38 +0300

    fix(cli/link): fix export

--- packages/cli/link/index.js
+++ packages/cli/link/index.js
@@ -1,5 +1,7 @@
 const { infon } = require('./log')
 const postLinkAndroid = require('./postLinkAndroid')
-postLinkAndroid()
 
-infon('startupjs link is completed. Thank you for using startupjs!\n\n')
+module.exports = function link () {
+  postLinkAndroid()
+  infon('startupjs link is completed. Thank you for using startupjs!\n\n')
+}
