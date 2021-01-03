28d8eef2cba666e250a2d11d6b815e7aa1a7bf08
Pavel Zhukov <cray0000@gmail.com>
2021-01-03 23:59:42 +0200

    feat(mdx): Rewrite pug template string highlighting to a full sub-language highlighting support. Support styl/css/pug highlighting as subtemplates.

--- packages/mdx/client/Code/index.js
+++ packages/mdx/client/Code/index.js
@@ -9,6 +9,8 @@ import languageMarkdown from 'refractor/lang/markdown.js'
 import languageJson from 'refractor/lang/json.js'
 import './index.styl'
 
+const SUB_LANGUAGE_REGEX = /(^|\W)(pug|styl|css)(`\s*\n)([^`]*\s*\n)(`)/
+
 // Register all supported languages
 refractor.register(languageJsx)
 refractor.register(languageStyl)
@@ -18,6 +20,48 @@ refractor.register(languageJson)
 
 // Register aliases
 refractor.alias({ stylus: ['styl'] })
+
+// This method mutates highlighted array to remove the last template
+// backtick symbol and also returns it
+function modifyAndGetLastBacktick (highlighted) {
+  if (!(highlighted && highlighted.length)) return []
+  const last = highlighted[highlighted.length - 1]
+  if (!last?.properties?.className?.includes('template-string')) {
+    throw new Error(`
+      [@startupjs/mdx] Last symbol is not a template-string.
+      This should never happen.
+      Maybe refractor got updated or <Code> component is broken.
+    `)
+  }
+  const lastClone = JSON.parse(JSON.stringify(last))
+  last.children.pop()
+  lastClone.children.splice(0, lastClone.children.length - 1)
+  return lastClone
+}
+
+function highlight (code, language) {
+  if (!code) return []
+  if (language === 'jsx') {
+    const match = code.match(SUB_LANGUAGE_REGEX)
+    if (match) {
+      const splitIndex = match.index + match[0].length
+      const start = code.slice(0, splitIndex)
+      const rest = code.slice(splitIndex)
+      const startJsx = start.replace(SUB_LANGUAGE_REGEX, '$1$2$3$5')
+      const startSubLanguage = match[4]
+      const subLanguage = match[2]
+      const startHighlightedJsx = refractor.highlight(startJsx, 'jsx')
+      const closingBacktick = modifyAndGetLastBacktick(startHighlightedJsx)
+      return [
+        ...startHighlightedJsx, // without trailing ` sign
+        ...highlight(startSubLanguage, subLanguage),
+        closingBacktick, // the trailing ` sign
+        ...highlight(rest, 'jsx')
+      ]
+    }
+  }
+  return refractor.highlight(code, language)
+}
 
 export default memo(function Code ({
   children = '',
@@ -30,17 +74,7 @@ export default memo(function Code ({
   const code = useMemo(() => {
     if (!language) return children
 
-    if (language === 'jsx') {
-      const pugIndex = children.search(/pug`/gi)
-
-      if (pugIndex !== -1) {
-        const jsPart = renderer(refractor.highlight(children.slice(0, pugIndex), language), textStyle)
-        const pugPart = renderer(refractor.highlight(children.slice(pugIndex), 'pug'), textStyle)
-        return [...jsPart, ...pugPart]
-      }
-    }
-
-    return renderer(refractor.highlight(children, language), textStyle)
+    return renderer(highlight(children, language), textStyle)
   }, [children, language])
 
   return pug`
