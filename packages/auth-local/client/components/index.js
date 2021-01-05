b847b42616fe293d879e780521ef37220d82f049
Vladyslav <aguretsvlad@gmail.com>
2021-01-05 13:55:35 +0200

    Trivial updates (small fixes) in auth (#464)
    * fix(auth): Add yes to reset password inputs
    
    * fix(auth): Add eyes icons to change password form
    
    * fix(auth): Some styls and import

--- packages/auth-local/client/components/index.js
+++ packages/auth-local/client/components/index.js
@@ -1,3 +1,4 @@
+export { default as TextInput } from './TextInput'
 export { default as LoginForm } from './LoginForm'
 export { default as RecoverForm } from './RecoverForm'
 export { default as RegisterForm } from './RegisterForm'
