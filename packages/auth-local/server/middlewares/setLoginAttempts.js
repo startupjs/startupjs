9e5b1c1dcb34d9471bc4444d15124c55758f8091
aguretsvlad <aguretsvlad@gmail.com>
2021-01-08 14:09:25 +0200

    fix(auth): Fix query to set login attempts

--- packages/auth-local/server/middlewares/setLoginAttempts.js
+++ packages/auth-local/server/middlewares/setLoginAttempts.js
@@ -5,11 +5,11 @@ const ALLOWED_FAILED_LOGIN_ATTEMPTS = 10
 export default async function setLoginAttempts (req, res, next) {
   const { model, body } = req
 
-  const $auths = model.query('auths', { email: body.email })
+  const $auths = model.query('auths', { 'providers.local.email': body.email })
   await $auths.subscribe()
   const authDoc = $auths.get()[0]
 
-  if (authDoc && authDoc.providers.local) {
+  if (authDoc) {
     const $auth = model.scope(`auths.${authDoc.id}`)
     const failedLoginAttempts = $auth.get('providers.local.failedLoginAttempts') || 0
 
