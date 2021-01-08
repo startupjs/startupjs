35e395d635f2ac994d8803941348e87914b3e73c
aguretsvlad <aguretsvlad@gmail.com>
2021-01-08 14:02:26 +0200

    feat(auth): Add redirectUrl for logout action

--- packages/auth/client/helpers/onLogout.web.js
+++ packages/auth/client/helpers/onLogout.web.js
@@ -1,7 +1,10 @@
+import { $root } from 'startupjs'
 import axios from 'axios'
 import { LOGOUT_URL } from '../../isomorphic'
 
-export default async function onLogout () {
+export default async function onLogout (redirectUrl) {
+  const _redirectUrl = redirectUrl || $root.get('_session.auth.signInPageUrl') || '/'
+
   await axios.get(LOGOUT_URL)
-  window.location.href = '/'
+  window.location.href = _redirectUrl
 }
