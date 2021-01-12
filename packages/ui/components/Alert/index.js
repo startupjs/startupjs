ffc271d834b2556fbc54e32674927709f494205f
lupenq <skinilya@gmail.com>
2021-01-12 18:51:19 +0300

    ref(ui/Alert): improve Alert

--- packages/ui/components/Alert/index.js
+++ packages/ui/components/Alert/index.js
@@ -1,40 +1,64 @@
 import React from 'react'
 import { observer } from 'startupjs'
 import PropTypes from 'prop-types'
+import {
+  faExclamationCircle,
+  faTimes,
+  faCheckCircle,
+  faExclamationTriangle,
+  faInfoCircle
+} from '@fortawesome/free-solid-svg-icons'
 import Div from '../Div'
 import Span from '../typography/Span'
 import Row from '../Row'
 import Icon from '../Icon'
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import './index.styl'
 
+const ICONS = {
+  info: faInfoCircle,
+  error: faExclamationCircle,
+  warning: faExclamationTriangle,
+  success: faCheckCircle
+}
 function Alert ({
   variant,
   icon,
   label,
+  title,
+  ActionComponent,
   onClose
 }) {
   return pug`
     Row.root(
-      vAlign='center'
+      vAlign='start'
       styleName=[variant]
     )
-      if icon
+      if icon !== false
         Div.iconWrapper
           Icon.icon(
-            icon=icon
+            icon=icon || ICONS[variant]
+            size='l'
             styleName=[variant]
           )
-      if label
-        Span.label(
-          styleName=[variant]
-          numberOfLines=1
-        )
-          = label
-      if onClose
+      Div.content
+        if title
+          Span.title(
+            bold
+            numberOfLines=1
+          )
+            = title
+        if label
+          Span.label(
+            numberOfLines=1
+          )
+            = label
+      if ActionComponent
+        ActionComponent
+      if !ActionComponent && onClose
         Div.closeIconWrapper(onPress=onClose)
           Icon.closeIcon(
             icon=faTimes
+            size='l'
             styleName=[variant]
           )
   `
@@ -46,8 +70,10 @@ Alert.defaultProps = {
 
 Alert.propTypes = {
   variant: PropTypes.oneOf(['info', 'error', 'warning', 'success']),
+  title: PropTypes.string,
   label: PropTypes.string,
-  icon: PropTypes.object,
+  icon: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
+  ActionComponent: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
   onClose: PropTypes.func
 }
 
