759da16146fbcdac067c76f67455c9435aec516e
ssstelsss <oqjwcqdncqbm@mail.ru>
2021-01-15 18:50:20 +0300

    feat(Detox): add android linker

--- packages/cli/link/postLinkAndroid.js
+++ packages/cli/link/postLinkAndroid.js
@@ -1,11 +1,14 @@
 const { infon, logn } = require('./log')
 const ActivityLinker = require('./activityLinker')
 const StylesLinker = require('./stylesLinker')
+const DetoxLinker = require('./DetoxLinker')
 
 module.exports = () => {
   infon('\nRunning Android postlink script\n')
   new ActivityLinker().link()
   logn()
   new StylesLinker().link()
+  logn()
+  new DetoxLinker().link()
   logn('\n')
 }
