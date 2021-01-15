20b003970297d5049494672e327a41b9ed50c0d1
simonprod <xeywar@gmail.com>
2021-01-15 14:11:49 +0300

    fix(ui/Dropdown): add style and disabled props in item (#481)

--- packages/ui/components/popups/Dropdown/components/Item/index.js
+++ packages/ui/components/popups/Dropdown/components/Item/index.js
@@ -9,10 +9,12 @@ import Link from '../../../../Link'
 import './index.styl'
 
 function DropdownItem ({
+  style,
   to,
   label,
   value,
   icon,
+  disabled,
   onPress,
   children,
   _activeValue,
@@ -38,10 +40,11 @@ function DropdownItem ({
     return pug`
       Menu.Item(
         to=to
+        style=style
         active=_activeValue === value
+        disabled=disabled
         styleName={ selectMenu: _selectIndexValue === _index }
         onPress=handlePress
-        iconPosition='left'
         icon=icon
       )= label
     `
@@ -49,7 +52,11 @@ function DropdownItem ({
 
   const Wrapper = to ? Link : TouchableOpacity
   return pug`
-    Wrapper(onPress=handlePress to=to)
+    Wrapper(
+      to=to
+      style=style
+      onPress=handlePress
+    )
       View.item(styleName=[!isPure && _variant, {
         active: !isPure && (_activeValue === value),
         itemUp: !isPure && (_index === 0),
