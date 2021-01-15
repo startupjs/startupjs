1601a3842e622370a83e639f0746ed91b6bbd383
lupenq <skinilya@gmail.com>
2021-01-15 14:52:37 +0300

    feat(Docs/Tutorial): add ru/en StartupJS Tutorial

--- packages/ui/docs/index.js
+++ packages/ui/docs/index.js
@@ -3,11 +3,13 @@ import hooksAndHelpers from './hooksAndHelpers'
 import foundation from './foundation'
 import forms from './forms'
 import popups from './popups'
+import tutorial from './tutorial'
 
 export default {
   foundation,
   components,
   forms,
   popups,
-  hooksAndHelpers
+  hooksAndHelpers,
+  tutorial
 }
