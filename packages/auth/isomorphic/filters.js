3c121cca8b3ab4f7e296758f2b3e4de1417a4506
vianormonroes <72992586+vianormonroes@users.noreply.github.com>
2021-01-15 13:42:34 +0300

    feat(auth) add filters to client routes for not logged in user (#484)
    * feat(login) add filters to client routes for not logged in user
    
    * [wip] work on feedback (change the success redirect url)

--- packages/auth/isomorphic/filters.js
+++ packages/auth/isomorphic/filters.js
@@ -1,6 +1,6 @@
 import { SIGN_IN_URL } from './constants'
 
-function isLoggedIn (signInPageUrl) {
+function isLoggedIn(signInPageUrl) {
   return function (model, next, redirect) {
     const loggedIn = model.get('_session.loggedIn')
     if (!loggedIn) return redirect(signInPageUrl || SIGN_IN_URL)
@@ -8,6 +8,17 @@ function isLoggedIn (signInPageUrl) {
   }
 }
 
+function isNotLoggedIn(redirectUrl) {
+  return function (model, next, redirect) {
+    const loggedIn = model.get('_session.loggedIn')
+    const successRedirectUrl =
+      redirectUrl || model.get('_session.auth.successRedirectUrl') || '/'
+    if (loggedIn) return redirect(successRedirectUrl)
+    next()
+  }
+}
+
 export default {
-  isLoggedIn
+  isLoggedIn,
+  isNotLoggedIn
 }
--- packages/auth/isomorphic/getAuthRoutes.js
+++ packages/auth/isomorphic/getAuthRoutes.js
@@ -1,22 +1,28 @@
+import filters from './filters'
+
 export default (components = {}) => [
   {
     path: '/auth/sign-in',
     exact: true,
-    component: components.PSignIn
+    component: components.PSignIn,
+    filters: [filters.isNotLoggedIn()]
   },
   {
     path: '/auth/sign-up',
     exact: true,
-    component: components.PSignUp
+    component: components.PSignUp,
+    filters: [filters.isNotLoggedIn()]
   },
   {
     path: '/auth/recover',
     exact: true,
-    component: components.PRecover
+    component: components.PRecover,
+    filters: [filters.isNotLoggedIn()]
   },
   {
     path: '/auth/reset-password',
     exact: true,
-    component: components.PResetPassword
+    component: components.PResetPassword,
+    filters: [filters.isNotLoggedIn()]
   }
 ]
