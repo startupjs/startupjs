265795a71283bb85ff64ec45e818a94f624d6df8
simonprod <xeywar@gmail.com>
2021-02-04 11:57:43 +0300

    feat(auth-apple): linking

--- packages/auth-apple/server/api/loginNative.js
+++ packages/auth-apple/server/api/loginNative.js
@@ -1,42 +1,38 @@
-import { finishAuth } from '@startupjs/auth/server'
+import { finishAuth, linkAccount } from '@startupjs/auth/server'
 import crypto from 'crypto'
 import appleSigninAuth from 'apple-signin-auth'
 import Provider from '../Provider'
 
 export default async function loginNative (req, res, config) {
+  const { successRedirectUrl, onBeforeLoginHook } = config
   const data = req.body
 
+  let profile
   if (data.code) {
-    loginAndroid(req, res, data, config)
+    profile = getAndroidProfile(data)
   } else {
-    loginIOS(req, res, data, config)
+    profile = data
+  }
+
+  const provider = new Provider(req.model, profile, config)
+
+  // If request came along with authorized session -> link new account to existing auth.providers doc
+  if (req.session.loggedIn) {
+    const response = await linkAccount(req, provider)
+    return res.send(response)
+  } else {
+    const userId = await provider.findOrCreateUser()
+    finishAuth(req, res, { userId, successRedirectUrl, onBeforeLoginHook })
   }
 }
 
-async function loginIOS (req, res, profile, config) {
-  const { successRedirectUrl, onBeforeLoginHook } = config
-
-  const provider = new Provider(req.model, profile, config)
-  const userId = await provider.findOrCreateUser()
-
-  finishAuth(req, res, { userId, successRedirectUrl, onBeforeLoginHook })
-}
-
-async function loginAndroid (req, res, data, config) {
-  const { successRedirectUrl, onBeforeLoginHook } = config
-
+async function getAndroidProfile (data) {
   const appleIdTokenClaims = await appleSigninAuth.verifyIdToken(data.id_token, {
     nonce: data.nonce
       ? crypto.createHash('sha256').update(data.nonce).digest('hex')
       : undefined
   })
 
-  const profile = {
-    id: appleIdTokenClaims.sub
-  }
-
-  const provider = new Provider(req.model, profile, config)
-  const userId = await provider.findOrCreateUser()
-
-  finishAuth(req, res, { userId, successRedirectUrl, onBeforeLoginHook })
+  const profile = { id: appleIdTokenClaims.sub }
+  return profile
 }
