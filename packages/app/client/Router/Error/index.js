367699dd10f8183918f6930911fa1b7be9760d79
ssstelsss <oqjwcqdncqbm@mail.ru>
2021-02-08 13:12:58 +0300

    feedback

--- packages/app/client/Router/Error/index.js
+++ packages/app/client/Router/Error/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import { View } from 'react-native'
 import { observer } from 'startupjs'
 import { defaultTemplates } from './templates'
-import './index.styl'
 
 export default observer(function Error ({ value, pages = {}, supportEmail }) {
   // TODO: Need to make the default layout better
   const Template = pages[value] || defaultTemplates[value] || defaultTemplates.default
   return pug`
-    View.root
-      Template(supportEmail=supportEmail)
+    Template(supportEmail=supportEmail)
   `
 })
