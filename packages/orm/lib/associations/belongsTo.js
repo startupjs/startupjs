3d08205cdbc8dc71acdcd1204a824d2eec413cbf
Pavel Turchik <pturchik@gmail.com>
2021-02-09 11:33:50 +0300

    feat(orm): set associations in both directions (parent and child) (#508)

--- packages/orm/lib/associations/belongsTo.js
+++ packages/orm/lib/associations/belongsTo.js
@@ -2,13 +2,25 @@ import pluralize from 'pluralize'
 
 export default function belongsTo (AssociatedOrmEntity, options) {
   return function (OrmEntity) {
-    OrmEntity.associations.push(
+    OrmEntity.addAssociation(
       Object.assign({
         type: 'belongsTo',
         orm: AssociatedOrmEntity,
-        key: pluralize.singular(AssociatedOrmEntity.collection) + 'Id'
+        key: pluralize.singular(AssociatedOrmEntity.collection) + 'Id',
+        childrenName: OrmEntity.collection
       }, options)
     )
+
+    AssociatedOrmEntity.addAssociation(
+      Object.assign({
+        type: 'oppositeBelongsTo',
+        orm: OrmEntity,
+        key: pluralize.singular(AssociatedOrmEntity.collection) + 'Id',
+        childrenName: OrmEntity.collection,
+        opposite: true
+      }, options)
+    )
+
     return OrmEntity
   }
 }
--- packages/orm/lib/associations/hasMany.js
+++ packages/orm/lib/associations/hasMany.js
@@ -2,13 +2,25 @@ import pluralize from 'pluralize'
 
 export default function hasMany (AssociatedOrmEntity, options) {
   return function (OrmEntity) {
-    OrmEntity.associations.push(
+    OrmEntity.addAssociation(
       Object.assign({
         type: 'hasMany',
         orm: AssociatedOrmEntity,
-        key: pluralize.singular(AssociatedOrmEntity.collection) + 'Ids'
+        key: pluralize.singular(AssociatedOrmEntity.collection) + 'Ids',
+        childrenName: AssociatedOrmEntity.collection
       }, options)
     )
+
+    AssociatedOrmEntity.addAssociation(
+      Object.assign({
+        type: 'oppositeHasMany',
+        orm: OrmEntity,
+        key: pluralize.singular(AssociatedOrmEntity.collection) + 'Ids',
+        childrenName: OrmEntity.collection, // TODO:
+        opposite: true
+      }, options)
+    )
+
     return OrmEntity
   }
 }
