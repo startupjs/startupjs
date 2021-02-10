a23e9255e8fd6ca41531e1e7cd134fdc088ec528
Oleksandr Pishchuk <46069436+olvipi@users.noreply.github.com>
2021-02-10 15:33:10 +0200

    update templates (#514)

--- packages/startupjs/templates/routing/main/Layout/index.js
+++ packages/startupjs/templates/routing/main/Layout/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { observer, emit } from 'startupjs'
 import { View, TouchableOpacity, Text } from 'react-native'
+import { observer, emit } from 'startupjs'
 import './index.styl'
 
 export default observer(function ({ children }) {
