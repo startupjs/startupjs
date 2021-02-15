f2de10bdd1bd35d46fca9ebcdacb5e327ad03b39
lupenq <skinilya@gmail.com>
2021-02-15 17:06:24 +0300

    fix(e2e): commen detox android linker

--- packages/cli/link/postLinkAndroid.js
+++ packages/cli/link/postLinkAndroid.js
@@ -1,14 +1,15 @@
 const { infon, logn } = require('./log')
 const ActivityLinker = require('./activityLinker')
 const StylesLinker = require('./stylesLinker')
-const DetoxLinker = require('./DetoxLinker')
+// uncomment when the android in Detox will be implemented
+// const DetoxLinker = require('./DetoxLinker')
 
 module.exports = () => {
   infon('\nRunning Android postlink script\n')
   new ActivityLinker().link()
   logn()
   new StylesLinker().link()
-  logn()
-  new DetoxLinker().link()
+  // logn()
+  // new DetoxLinker().link()
   logn('\n')
 }
