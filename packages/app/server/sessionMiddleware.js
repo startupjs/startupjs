cc1b0ee73522465ec9c141cc3fb75b29676beea7
simonprod <xeywar@gmail.com>
2021-03-03 00:04:36 +0300

    fix route

--- packages/app/server/sessionMiddleware.js
+++
@@ -1,7 +0,0 @@
-export default function sessionMiddleware (req, res, next) {
-  const restoreUrl = req.session.restoreUrl
-  if (!restoreUrl || req.url !== '/api/serverSession') return next()
-  delete req.session.restoreUrl
-  req.model.set('_session.restoreUrl', restoreUrl)
-  next()
-}
