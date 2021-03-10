47937aba16735949cdd7ac00c445d118efc79052
simonprod <xeywar@gmail.com>
2021-03-10 18:14:39 +0300

    add wrapper component

--- packages/plugin/client/Slot.js
+++ packages/plugin/client/Slot.js
@@ -10,18 +10,31 @@ export default observer(function Slot ({
   const plugins = usePlugins()
   const options = useOptions()
 
-  return plugins.reduce((acc, pluginStructure) => {
+  return plugins.reduce((recursiveChildren, pluginStructure) => {
     const Component = pluginStructure[name]
     const pluginOptions = options[pluginStructure.name]
 
     if (Component) {
-      return useMemo(() => {
-        return pug`
-          Component(...props options=pluginOptions)= acc
-        `
-      }, [JSON.stringify(pluginOptions), children])
+      return pug`
+        WrapperComponent(
+          Component=Component
+          options=pluginOptions
+          ...props
+        )= recursiveChildren
+      `
     }
 
-    return acc
+    return recursiveChildren
   }, children)
+})
+
+const WrapperComponent = observer(({
+  Component,
+  options,
+  children,
+  ...props
+}) => {
+  return useMemo(() => pug`
+    Component(options ...props)= children
+  `, [JSON.stringify(options), children])
 })
