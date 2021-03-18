1dd7741667491efba65fef655f37a90aaaf3a035
ssstelsss <oqjwcqdncqbm@mail.ru>
2021-03-18 12:35:40 +0300

    feat(2fa): add 2fa package

---
+++ packages/2fa/client/helpers/getSecret.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import { GET_SECRET_URL } from '../../isomorphic'
+
+export default async function getSecret (token) {
+  try {
+    const secret = await axios.get(GET_SECRET_URL)
+    return secret.data
+  } catch (err) {
+    console.log('err: ', err.response.data)
+  }
+}
