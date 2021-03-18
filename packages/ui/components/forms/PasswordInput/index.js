f2cbe8b495086e9f5e0a37b8a104467d4d99fb84
Oleksandr Pishchuk <ovpischuk@gmail.com>
2021-03-18 17:38:58 +0200

    fix(PasswordInput): change secureTextEntry value

--- packages/ui/components/forms/PasswordInput/index.js
+++ packages/ui/components/forms/PasswordInput/index.js
@@ -1,20 +1,36 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { observer } from 'startupjs'
+import PropTypes from 'prop-types'
+import omit from 'lodash/omit'
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import TextInput from '../TextInput'
 
-export default function PasswordInput ({
-  secureTextEntry = false,
-  ...props
-}) {
-  const [showText, setShowText] = useState(secureTextEntry)
+function PasswordInput ({ secureTextEntry, ...props }) {
+  const [textHidden, setTextHidden] = useState(true)
+
+  useEffect(() => {
+    // HACK: it is important for the WEB to pass by default
+    // the secureTextEntry=true property to the TextInput,
+    // because the field will not automatically accept
+    // the password stored for the domain
+    if (typeof secureTextEntry === 'boolean') setTextHidden(secureTextEntry)
+  }, [secureTextEntry])
 
   return pug`
     TextInput(
       ...props
-      secureTextEntry=!showText
-      icon=showText ? faEyeSlash : faEye
+      autoCompleteType='password'
+      secureTextEntry=textHidden
+      icon=textHidden ? faEye : faEyeSlash
       iconPosition='right'
-      onIconPress=() => setShowText(!showText)
+      onIconPress=() => setTextHidden(!textHidden)
     )
   `
 }
+
+PasswordInput.propTypes = {
+  ...omit(TextInput.propTypes, ['icon', 'iconPosition', 'numberOfLines', 'resize', 'onIconPress']),
+  secureTextEntry: PropTypes.bool
+}
+
+export default observer(PasswordInput)
