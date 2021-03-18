e86d6343eb311529a47edd0b466464a319164ed5
ssstelsss <oqjwcqdncqbm@mail.ru>
2021-03-18 18:56:36 +0300

    feat(startupjs/tabs): update react-native-tab-view to 3.0.0

--- packages/cli/link/postLinkAndroid.js
+++ packages/cli/link/postLinkAndroid.js
@@ -1,13 +1,14 @@
 const { infon, logn } = require('./log')
-const ActivityLinker = require('./activityLinker')
+// it isn't need at currunt version becouse we use react-native-tab-view@3.0.0
+// const ActivityLinker = require('./activityLinker')
 const StylesLinker = require('./stylesLinker')
 // uncomment when the android in Detox will be implemented
 // const DetoxLinker = require('./DetoxLinker')
 
 module.exports = () => {
   infon('\nRunning Android postlink script\n')
-  new ActivityLinker().link()
-  logn()
+  // new ActivityLinker().link()
+  // logn()
   new StylesLinker().link()
   // logn()
   // new DetoxLinker().link()
