6af81ff6b3f27486c07dd32bb8a33c996d4d428a
ssstelsss <oqjwcqdncqbm@mail.ru>
2021-03-22 18:32:31 +0300

    minor

--- packages/2fa/server/helpers/index.js
+++ packages/2fa/server/helpers/index.js
@@ -16,7 +16,10 @@ export async function createOrUpdateSecret (model, session, options) {
   const $auths = model.scope(`auths.${userId}`)
   await $auths.subscribe()
 
-  if (!$auths.get()) throw new Error('User is not logged in with @startupjs/auth')
+  if (!$auths.get()) {
+    $auths.unsubscribe()
+    throw new Error('User is not logged in with @startupjs/auth')
+  }
 
   $auths.set('providers.2fa', { ...secretObj })
 
@@ -53,11 +56,11 @@ export async function getSecret (model, session) {
   await $auths.subscribe()
   const secret = $auths.get('providers.2fa')
 
+  $auths.unsubscribe()
+
   if (!secret) throw new Error('Two factor authentication has not connected!')
 
   const QRDataURL = await QRCode.toDataURL(secret.otpauthUrl)
-
-  $auths.unsubscribe()
 
   return {
     base32: secret.base32,
