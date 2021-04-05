80d2a0b2486037b53158585b0a5fc038c6e56636
lupenq <skinilya@gmail.com>
2021-04-05 16:17:00 +0300

    feat(recaptcha): add recaptchaSecretKeyExists to session

--- packages/recaptcha/server/middlewares/setRecaptchaSiteKey.js
+++ packages/recaptcha/server/middlewares/setRecaptchaSiteKey.js
@@ -4,13 +4,19 @@ export default function setRecaptchaSiteKey (req, res, next) {
   const $session = req.model.scope('_session')
 
   const RECAPTCHA_SITE_KEY = nconf.get('RECAPTCHA_SITE_KEY')
+  const recaptchaSecretKeyExists = !!nconf.get('RECAPTCHA_SECRET_KEY')
 
   if (!RECAPTCHA_SITE_KEY) {
     console.warn('\x1b[33m%s\x1b[0m', '[@startupjs/recaptcha] WARNING: The "RECAPTCHA_SITE_KEY" field in config.json is missing')
   }
 
+  if (!recaptchaSecretKeyExists) {
+    console.warn('\x1b[33m%s\x1b[0m', '[@startupjs/recaptcha] WARNING: The "RECAPTCHA_SECRET_KEY" field in config.json is missing')
+  }
+
   try {
     $session.set('Recaptcha.RECAPTCHA_SITE_KEY', RECAPTCHA_SITE_KEY)
+    $session.set('Recaptcha.recaptchaSecretKeyExists', recaptchaSecretKeyExists)
   } catch (err) {
     return next(err)
   }
