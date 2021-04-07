a2bf22b9ff2e4c0a8100173101ceb10b42c1ac39
ssstelsss <oqjwcqdncqbm@mail.ru>
2021-04-07 18:51:24 +0300

    minor

--- packages/notification/client/helpers/index.js
+++ packages/notification/client/helpers/index.js
@@ -1,3 +1,3 @@
 export { default as sendNotification } from './sendNotification'
 export { default as initOneSignal } from './initOneSignal'
-export { default as OneSignal } from './OneSignal'
+export { OneSignal } from './OneSignal'
