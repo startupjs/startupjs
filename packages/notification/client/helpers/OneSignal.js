50ed19902d8cae7484450a691f267d58ac081204
ssstelsss <oqjwcqdncqbm@mail.ru>
2021-04-07 11:08:21 +0300

    add OneSignal insatnce

---
+++ packages/notification/client/helpers/OneSignal.js
@@ -0,0 +1,1 @@
+export { default as OneSignal } from 'react-native-onesignal'
--- packages/notification/client/helpers/index.js
+++ packages/notification/client/helpers/index.js
@@ -1,2 +1,3 @@
 export { default as sendNotification } from './sendNotification'
 export { default as initOneSignal } from './initOneSignal'
+export { default as OneSignal } from './OneSignal'
