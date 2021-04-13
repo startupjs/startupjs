5b341cd4c4bb938f6832b35184a4796335093075
ssstelsss <oqjwcqdncqbm@mail.ru>
2021-04-13 11:53:57 +0300

    rename oneSignalInstance to oneSignalAxiosInstance

--- packages/push-notifications/server/helpers/index.js
+++ packages/push-notifications/server/helpers/index.js
@@ -4,14 +4,14 @@ import nconf from 'nconf'
 const ONESIGNAL_REST_KEY = nconf.get('ONESIGNAL_REST_KEY')
 const ONESIGNAL_APP_ID = nconf.get('ONESIGNAL_APP_ID')
 
-const onesignalInstance = axios.create({
+const oneSignalAxiosInstance = axios.create({
   baseURL: 'https://onesignal.com/api/v1/notifications',
   timeout: 1000,
   headers: { Authorization: `Basic ${ONESIGNAL_REST_KEY}` }
 })
 
 export async function sendNotification (data) {
-  await onesignalInstance.post('/', {
+  await oneSignalAxiosInstance.post('/', {
     ...data,
     app_id: ONESIGNAL_APP_ID
   })
