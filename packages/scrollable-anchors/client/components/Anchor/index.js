a917b341d0c421b5f989c6dd105dfa02192bf2f9
aguretsvlad <aguretsvlad@gmail.com>
2021-04-15 19:01:16 +0200

    fix(): Recalc anchors pos on height change

--- packages/scrollable-anchors/client/components/Anchor/index.js
+++ packages/scrollable-anchors/client/components/Anchor/index.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react'
-import { observer } from 'startupjs'
+import React, { useEffect, useState } from 'react'
+import { observer, useOn } from 'startupjs'
 import { Div } from '@startupjs/ui'
 import PropTypes from 'prop-types'
 import { registerAnchor, unregisterAnchor } from '../../helpers'
-
 function Anchor ({ id, children, style, Component, ...componentProps }) {
+  const [key, setKey] = useState(0)
+
   function _onLayout (e) {
     const { nativeEvent: { layout } } = e
     registerAnchor({
@@ -21,10 +22,18 @@ function Anchor ({ id, children, style, Component, ...componentProps }) {
     unregisterAnchor(id)
   }
 
+  // To force onLayout trigger to recalculate positions on list height change
+  function rerender () {
+    setKey(key + 1)
+  }
+
+  useOn('ScrollableProvider.recalcPositions', rerender)
+
   useEffect(() => onUnmount, [])
   return pug`
     Component(
       ...componentProps
+      key=key
       onLayout=_onLayout
       style=style
     )
