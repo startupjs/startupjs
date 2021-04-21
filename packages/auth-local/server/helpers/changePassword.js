a9510059210021c04b520cc04d91d02fc7e174ad
aguretsvlad <aguretsvlad@gmail.com>
2021-04-21 16:08:42 +0200

    refactor(auth): Divide change pass logic

---
+++ packages/auth-local/server/helpers/changePassword.js
@@ -0,0 +1,33 @@
+import bcrypt from 'bcrypt'
+import _get from 'lodash/get'
+
+export default async function changePassword ({ model, userId, password }) {
+  if (!userId) {
+    throw new Error('Provide user id')
+  }
+
+  if (!password) {
+    throw new Error('Provide password')
+  }
+
+  const $auth = model.scope(`auths.${userId}`)
+  await $auth.fetch()
+
+  const auth = $auth.get()
+
+  if (!auth) {
+    throw new Error('User not found')
+  }
+
+  const newSalt = await bcrypt.genSalt(10)
+  const newHash = await bcrypt.hash(password, newSalt)
+
+  if (!_get(auth, 'providers.local')) {
+    throw new Error('User has no local auth provider')
+  }
+
+  await $auth.setAsync('providers.local.hash', newHash)
+  await $auth.setAsync('providers.local.salt', newSalt)
+
+  model.unfetch($auth)
+}
