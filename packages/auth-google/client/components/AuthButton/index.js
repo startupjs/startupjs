c081dca09ab10d534448b2ecaafccba62836fe46
Ilya Lupen <skinilya@gmail.com>
2021-04-27 17:02:58 +0300

    feat(auth-google): pass login options to google auth library (#634)

--- packages/auth-google/client/components/AuthButton/index.js
+++ packages/auth-google/client/components/AuthButton/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { observer, useSession } from 'startupjs'
+import { observer } from 'startupjs'
 import { Button } from '@startupjs/ui'
 import PropTypes from 'prop-types'
 import { faGoogle } from '@fortawesome/free-brands-svg-icons'
@@ -8,27 +8,15 @@ import './index.styl'
 
 function AuthButton ({
   style,
-  baseUrl,
-  redirectUrl,
-  label
+  label,
+  ...options
 }) {
-  const [authConfig] = useSession('auth')
-  const { expiresRedirectUrl } = authConfig
-
-  function _onLogin () {
-    onLogin({
-      baseUrl,
-      redirectUrl,
-      expiresRedirectUrl
-    })
-  }
-
   return pug`
     Button.button(
       style=style
       icon=faGoogle
       variant='flat'
-      onPress=_onLogin
+      onPress=() => onLogin(options)
     )= label
   `
 }
