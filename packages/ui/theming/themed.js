28ba10b110b461c5b683017edb956ddf7e505261
Pavel Zhukov <cray0000@gmail.com>
2021-05-21 06:57:58 +0300

    feat(ui): Add ability to override ui styles by passing 'style' option to the uiAppPlugin (see styleguide/Root/index.js for the example)

--- packages/ui/theming/themed.js
+++ packages/ui/theming/themed.js
@@ -1,9 +1,36 @@
 import React, { useContext } from 'react'
+import matcher from '@startupjs/babel-plugin-rn-stylename-to-style/matcher'
 import ThemeContext from './ThemeContext'
+import { useStyle } from '../StyleContext'
 
 // TODO: Move themed inside react-sharedb's observer()
-export default function themed (Component) {
+export default function themed (name, Component) {
+  if (typeof name !== 'string') {
+    Component = name
+    name = Component.displayName || Component.name
+  }
   function ThemeWrapper (props) {
+    // Setup global component style overrides
+    const uiStyle = useStyle()
+    if (uiStyle) {
+      const styleProps = matcher(name, uiStyle, '', '', {}) || {}
+      const keysLength = Object.keys(styleProps).length
+      if (
+        keysLength > 0 &&
+        // Handle special case when we have an empty style array
+        // TODO: Fix returning an empty style array
+        !(keysLength === 1 && Array.isArray(styleProps.style) && styleProps.style.length === 0)
+      ) {
+        for (const key in styleProps) {
+          if (props[key]) {
+            styleProps[key] = [styleProps[key], props[key]]
+          }
+        }
+        props = { ...props, ...styleProps }
+      }
+    }
+
+    // Setup theme context
     let contextTheme = useContext(ThemeContext)
     let theme = props.theme || contextTheme
     let res
