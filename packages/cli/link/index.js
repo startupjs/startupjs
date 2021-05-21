5409695f38145eb9c9eeb2aa8de1f69ee492682f
ssstelsss <oqjwcqdncqbm@mail.ru>
2021-05-21 14:58:09 +0300

    feat(cli): create abstract command link insted android-link

--- packages/cli/link/index.js
+++ packages/cli/link/index.js
@@ -1,7 +1,21 @@
 const { infon } = require('./log')
 const postLinkAndroid = require('./postLinkAndroid')
 
-module.exports = function link () {
-  postLinkAndroid()
+module.exports = function link (options = {}) {
+  // default case, when there is no flags
+  if (!Object.keys(options).length) {
+    postLinkAndroid()
+    return endLinking()
+  }
+
+  const { standart } = options
+
+  if (standart) {
+    postLinkAndroid()
+  }
+  endLinking()
+}
+
+function endLinking () {
   infon('startupjs link is completed. Thank you for using startupjs!\n\n')
 }
