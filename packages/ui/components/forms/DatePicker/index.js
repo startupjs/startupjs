d0007017b9220b43a8001a83fac68c636127f988
simonprod <xeywar@gmail.com>
2021-06-25 11:52:48 +0300

    fix

--- packages/ui/components/forms/DatePicker/index.js
+++ packages/ui/components/forms/DatePicker/index.js
@@ -1,8 +1,11 @@
 import React, { useMemo, useRef, useCallback, useState, useEffect } from 'react'
 import { observer, useValue } from 'startupjs'
-import { Div, Divider, TextInput, Popover } from '@startupjs/ui'
 import PropTypes from 'prop-types'
 import moment from 'moment'
+import Div from '../../Div'
+import Divider from '../../Divider'
+import TextInput from '../TextInput'
+import Popover from '../../popups/Popover'
 import 'moment/min/locales'
 import getLocale from './getLocale'
 import Calendar from './Calendar'
@@ -91,7 +94,52 @@ function DatePicker ({
     setTextInput(text)
   }
 
-  // mobile to Drawer
+  const caption = pug`
+    if renderCaption
+      = renderCaption()
+    else
+      TextInput(
+        ref=refInput
+        style=style
+        disabled=disabled
+        label=label
+        size=size
+        placeholder=placeholder
+        value=visible ? textInput : getFormatDate()
+        onFocus=onFocus
+        onChangeText=onChangeText
+      )
+  `
+
+  const content = pug`
+    Div.content
+      if (mode === 'date') || (mode === 'datetime')
+        Calendar(
+          date=date
+          exactLocale=exactLocale
+          disabledDays=disabledDays
+          locale=locale
+          maxDate=maxDate
+          minDate=minDate
+          range=range
+          timezone=timezone
+          onChangeDate=_onChangeDate
+        )
+
+      if mode === 'datetime'
+        Divider.divider(variant='vertical')
+
+      if (mode === 'time') || (mode === 'datetime')
+        TimeSelect(
+          date=date
+          timezone=timezone
+          exactLocale=exactLocale
+          is24Hour=_is24Hour
+          hourInterval=hourInterval
+          minuteInterval=minuteInterval
+          onChangeDate=_onChangeDate
+        )
+  `
 
   // TODO: New API Popover
   return pug`
@@ -99,54 +147,13 @@ function DatePicker ({
       visible=visible
       onDismiss=onDismiss
     )
-      Popover.Caption
-        if renderCaption
-          = renderCaption()
-        else
-          TextInput(
-            ref=refInput
-            style=style
-            disabled=disabled
-            label=label
-            size=size
-            placeholder=placeholder
-            value=visible ? textInput : getFormatDate()
-            onFocus=onFocus
-            onChangeText=onChangeText
-          )
-
-      Div.content
-        if (mode === 'date') || (mode === 'datetime')
-          Calendar(
-            date=date
-            exactLocale=exactLocale
-            disabledDays=disabledDays
-            locale=locale
-            maxDate=maxDate
-            minDate=minDate
-            range=range
-            timezone=timezone
-            onChangeDate=_onChangeDate
-          )
-
-        if mode === 'datetime'
-          Divider.divider(variant='vertical')
-
-        if (mode === 'time') || (mode === 'datetime')
-          TimeSelect(
-            date=date
-            timezone=timezone
-            exactLocale=exactLocale
-            is24Hour=_is24Hour
-            hourInterval=hourInterval
-            minuteInterval=minuteInterval
-            onChangeDate=_onChangeDate
-          )
+      Popover.Caption= caption
+      = content
   `
 }
 
 DatePicker.defaultProps = {
-  mode: 'time',
+  mode: 'datetime',
   size: 'm',
   maxDate: moment().add(100, 'year').valueOf(),
   hourInterval: 1,
