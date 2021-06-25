4aae10a03261a8fce3b90ecce69825a18aca9651
lilhakk <xeywar@gmail.com>
2021-06-25 11:27:54 +0300

    fix(app): move dialogs to ui library (#707)

--- packages/app/client/helpers/index.js
+++ packages/app/client/helpers/index.js
@@ -1,4 +1,2 @@
 export { default as useMediaUpdate } from './useMediaUpdate'
 export { default as useNeedUpdate } from './useNeedUpdate'
-export { alert, confirm, prompt } from './dialogs'
-export { getScope, usePage } from './path'
