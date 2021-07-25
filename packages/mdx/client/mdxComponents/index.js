c50aac7da30938ca30362ccee5f3d859bc45f901
Oleksandr Pishchuk <ovpischuk@gmail.com>
2021-07-25 23:09:46 +0300

    fix(mdx): fix link with anchor after scrolling

--- packages/mdx/client/mdxComponents/index.js
+++ packages/mdx/client/mdxComponents/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from 'react'
 import { Image, Platform } from 'react-native'
 import Clipboard from '@react-native-clipboard/clipboard'
-import { observer, useValue } from 'startupjs'
+import { $root, observer, useValue } from 'startupjs'
 import {
   Div,
   H2,
@@ -22,7 +22,7 @@ import {
   Collapse,
   Tooltip
 } from '@startupjs/ui'
-import { Anchor } from '@startupjs/scrollable-anchors'
+import { Anchor, scrollTo } from '@startupjs/scrollable-anchors'
 import { faLink, faCode, faCopy } from '@fortawesome/free-solid-svg-icons'
 import _kebabCase from 'lodash/kebabCase'
 import _get from 'lodash/get'
@@ -230,8 +230,21 @@ export default {
   th: Th,
   delete: P,
   a: ({ children, href }) => {
+    function onPress () {
+      const { url, hash } = $root.get('$render')
+      const [_url, _hash] = href.split('#')
+      if (url === _url && hash === `#${_hash}`) {
+        scrollTo({ anchorId: _hash })
+      }
+    }
+
     return pug`
-      Link.link(to=href size='l' color='primary')= children
+      Link.link(
+        to=href
+        size='l'
+        color='primary'
+        onPress=onPress
+      )= children
     `
   },
   img: ({ src }) => {
