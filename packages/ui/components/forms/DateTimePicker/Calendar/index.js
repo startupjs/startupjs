8c951848b54afde18d6248b65df82aea58722bf9
lilhakk <xeywar@gmail.com>
2021-07-26 22:16:10 +0300

    ref(ui/DatePicker): timezone (#701)
    BREAKING CHANGE:
    
    ### `startupjs/ui/DateTimePicker`
    
    - prop `format` rename to `dateFormat`
    - prop `cancelButtonText` remove, so there is no more cancel button
    - prop `confirmButtonText` remove, so there is no more confirm button

---
+++ packages/ui/components/forms/DateTimePicker/Calendar/index.js
@@ -0,0 +1,63 @@
+import React, { useCallback } from 'react'
+import { observer, useValue } from 'startupjs'
+import { Div } from '@startupjs/ui'
+import moment from 'moment'
+import Header from './Header'
+import { Days, Months, Years } from './slides'
+import 'moment/min/locales'
+
+const SLIDES = {
+  days: Days,
+  months: Months,
+  years: Years
+}
+
+function Calendar ({
+  date,
+  disabledDays = [],
+  exactLocale,
+  timezone,
+  maxDate,
+  minDate,
+  range,
+  onChangeDate
+}) {
+  const [uiDate, $uiDate] = useValue(date)
+  const [slide, $slide] = useValue('days')
+
+  const onJump = useCallback((unitKey, value) => {
+    const timestamp = +moment
+      .tz(uiDate, timezone)
+      .set(unitKey, value)
+
+    $uiDate.set(timestamp)
+    $slide.set('days')
+  }, [uiDate, timezone])
+
+  const Slide = SLIDES[slide]
+
+  return pug`
+    Div
+      Header(
+        uiDate=uiDate
+        exactLocale=exactLocale
+        timezone=timezone
+        $slide=$slide
+        $uiDate=$uiDate
+      )
+
+      Slide(
+        uiDate=uiDate
+        exactLocale=exactLocale
+        timezone=timezone
+        range=range
+        minDate=minDate
+        maxDate=maxDate
+        disabledDays=disabledDays
+        onJump=onJump
+        onChangeDate=onChangeDate
+      )
+  `
+}
+
+export default observer(Calendar)
