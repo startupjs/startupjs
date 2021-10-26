779969c50e2cd965da2f2aa635ddb6ebcf1b8e11
Pavel Zhukov <cray0000@gmail.com>
2021-10-26 19:58:21 +0300

    minor: fix vite-plugin-startupjs extensions

--- packages/vite-plugin-startupjs/index.js
+++ packages/vite-plugin-startupjs/index.js
@@ -1,8 +1,8 @@
-import jsPlugin from './jsPlugin'
-import stylPlugin from './stylPlugin'
-import configPlugin from './configPlugin'
-import observerPlugin from './observerPlugin'
-import htmlPlugin from './htmlPlugin'
+import jsPlugin from './jsPlugin.js'
+import stylPlugin from './stylPlugin.js'
+import configPlugin from './configPlugin.js'
+import observerPlugin from './observerPlugin.js'
+import htmlPlugin from './htmlPlugin.js'
 
 export default function viteTransformStartupjs (options) {
   return [
