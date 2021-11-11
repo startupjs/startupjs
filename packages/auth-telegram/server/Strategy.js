602bcc9c6fc052b8e538fd3738ca9111b5252a78
zheoreh <oeswww@gmail.com>
2021-11-11 14:26:36 +0300

    Implements telegram authentication on web

---
+++ packages/auth-telegram/server/Strategy.js
@@ -0,0 +1,44 @@
+import passport from 'passport'
+import { TelegramStrategy as Strategy } from 'passport-telegram-official'
+import initRoutes from './initRoutes'
+import Provider from './Provider'
+
+export default function (config = {}) {
+  this.config = {}
+
+  const func = ({ model, router, updateClientSession, authConfig }) => {
+    Object.assign(this.config, {
+      ...authConfig
+      // Any defaults....
+    }, config)
+
+    if (!config.botToken) {
+      throw new Error('[@dmapper/auth-telegram] Error:', 'Provide botToken')
+    }
+
+    const { botToken } = this.config
+
+    initRoutes({ router, config: this.config })
+
+    console.log('++++++++++ Initialization of Telegram auth strategy ++++++++++\n')
+
+    passport.use(
+      new Strategy({
+        botToken
+      },
+      async (profile, cb) => {
+        let provider, err
+        try {
+          provider = new Provider(model, profile, this.config)
+        } catch (e) {
+          err = e
+        }
+
+        return cb(err, provider)
+      })
+    )
+  }
+
+  func.providerName = 'telegram'
+  return func
+}
