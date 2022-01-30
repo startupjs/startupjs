0c392c410006fd4c4affa721db73393d3bf43bdc
Pavel Zhukov <cray0000@gmail.com>
2022-01-30 04:28:11 +0200

    minor(react-sharedb-hooks/model): adjust behaviour to latest hooks api. TODO: figure out what to do with .subscribeSync() on server, move types and typeFns from class and hooks to isomorphic package.

--- packages/react-sharedb-hooks/lib/model.js
+++ packages/react-sharedb-hooks/lib/model.js
@@ -26,6 +26,7 @@ export default function (racer) {
 function generateMethodOfType (typeFn) {
   const isQuery = typeFn === subQuery
   const isSync = typeFn === subLocal || typeFn === subValue
+  const takeOriginalModel = typeFn === subDoc || typeFn === subLocal
 
   // IMPORTANT: subLocal, subValue can actually be made to be synchronous,
   //            but for consistency of the sub* functions api, the decision was made
@@ -52,7 +53,14 @@ function generateMethodOfType (typeFn) {
     // For Doc, Local, Value return the model scoped to the hook path
     // But only after the initialization actually finished, otherwise
     // the ORM won't be able to properly resolve the path which was not referenced yet
-    const $model = !isQuery ? $subs.at(subId) : undefined
+    const $model = (() => {
+      if (isQuery) return
+      if (takeOriginalModel) {
+        return this.scope(getPath(params))
+      } else {
+        return $subs.at(subId)
+      }
+    })()
 
     // In any situation force access data through the object key to let observer know that the data was accessed
     const data = $subs.get()[subId]
@@ -67,6 +75,21 @@ function generateMethodOfType (typeFn) {
       $queryCollection || $model
     ]
   })
+}
+
+export function getPath (params) {
+  if (!params) {
+    // This should never happen
+    console.warn('[react-sharedb] Unknown Param')
+    return '__ERROR__.unknownParam'
+  }
+  const type = params.__subscriptionType
+  switch (type) {
+    case 'Local':
+      return params.params
+    case 'Doc':
+      return params.params && params.params.join('.')
+  }
 }
 
 export function getCollectionName (params) {
