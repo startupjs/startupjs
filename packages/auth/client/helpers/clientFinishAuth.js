7c02284b1fdcf76da7e8b28ad98f6def73996d9b
Asie <asie.kursaitova@gmail.com>
2022-02-11 10:34:07 +0300

    Edit emit

--- packages/auth/client/helpers/clientFinishAuth.js
+++ packages/auth/client/helpers/clientFinishAuth.js
@@ -2,6 +2,7 @@ import { $root, emit } from 'startupjs'
 
 export default async function clientFinishAuth (redirectUrl) {
   if (!$root.get('_session.loggedIn')) {
-    emit('restart', redirectUrl)
+    emit('url', redirectUrl)
+    emit('restart')
   }
 }
