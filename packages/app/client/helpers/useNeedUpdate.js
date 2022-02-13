bc3036f4fa7a1745faf208dade17a035813344f7
Pavel Zhukov <cray0000@gmail.com>
2022-02-13 15:04:49 +0200

    minor, remove debugging stuff

--- packages/app/client/helpers/useNeedUpdate.js
+++ packages/app/client/helpers/useNeedUpdate.js
@@ -3,19 +3,10 @@ import { useDoc } from 'startupjs'
 
 const OS = Platform.OS
 
-let resolved
-
-const promise = new Promise(resolve => {
-  setTimeout(() => {
-    resolved = true
-    resolve()
-  }, 1000)
-})
-
 export default function useNeedUpdate (criticalVersion) {
-  if (!resolved) throw promise
   const [, $version] = useDoc('service', 'version')
   const newOsVersion = $version.get(`criticalVersion.${OS}`)
   const currentOsVersion = criticalVersion && criticalVersion[OS]
+
   return currentOsVersion < newOsVersion
 }
