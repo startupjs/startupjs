259cbf2aff51f211c58dc33ef177ef9ac4f89d2b
Nariman <53316420+narimandev@users.noreply.github.com>
2022-03-21 16:30:09 +0400

    feat(auth): handling google auth token error (#922)

---
+++ packages/auth/client/app/pages/PError/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { observer, emit, useLocal } from 'startupjs'
+import { Span, Card, Button } from '@startupjs/ui'
+import './index.styl'
+
+function PError () {
+  const [query] = useLocal('$render.query')
+  return pug`
+    Card.root(variant='outlined')
+      Span.title= query.err
+      Button.btn(onPress=() => emit('url', '/')) ◀︎  Go back
+  `
+}
+
+export default observer(PError)
