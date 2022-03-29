e1d26bca89f0be548ebf1ed34df21c56a8eb5fd5
zheoreh <oeswww@gmail.com>
2022-03-30 01:39:19 +0300

    fix(auth-lti): passing school name to school entry

--- packages/auth-lti/server/helpers.js
+++ packages/auth-lti/server/helpers.js
@@ -8,6 +8,7 @@ export async function getDbSchools (model, collectionName) {
   const data = {}
   for (const school of schools) {
     data[school.name] = {
+      name: school.name,
       consumerKey: school.key,
       consumerSecret: school.secret,
       redirect: school.redirect
