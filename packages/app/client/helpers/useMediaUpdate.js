6d8a97fee79ee4a8d23d2418e72dfe32c727a339
Pavel Zhukov <cray0000@gmail.com>
2022-05-22 23:11:20 +0300

    fix(babel-plugin-rn-stylename-to-style): Fix dynamic updates of CSS @media when cache is used

--- packages/app/client/helpers/useMediaUpdate.js
+++ packages/app/client/helpers/useMediaUpdate.js
@@ -1,12 +1,15 @@
 import { useEffect } from 'react'
 import { Dimensions } from 'react-native'
-import debounce from 'lodash/debounce'
 import dimensions from '@startupjs/babel-plugin-rn-stylename-to-style/dimensions'
+import debounce from 'lodash/debounce'
 
 const DIMENSIONS_UPDATE_DELAY = 200
 
 const debouncedChangeDimensions = debounce(({ window }) => {
-  if (dimensions.width !== window.width) dimensions.width = window.width
+  if (dimensions.width !== window.width) {
+    console.log('> update dimensions width:', window.width)
+    dimensions.width = window.width
+  }
 }, DIMENSIONS_UPDATE_DELAY, { leading: false, trailing: true })
 
 export default function useMediaUpdate () {
