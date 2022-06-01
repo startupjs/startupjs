9ec3d6b1ac4acf91e6a0eff9448cf53f417971c1
zheoreh <oeswww@gmail.com>
2022-06-01 13:35:14 +0300

    fix(ui/RangeInput): removes measurement for labels

--- packages/ui/components/forms/RangeInput/Label/index.js
+++ packages/ui/components/forms/RangeInput/Label/index.js
@@ -1,8 +1,8 @@
-import React, { useLayoutEffect, useRef, useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Div from '../../../Div'
 import Span from '../../../typography/Span'
-import './index.styl'
+import styles from './index.styl'
 
 function Label ({
   oneMarkerValue,
@@ -12,26 +12,15 @@ function Label ({
   oneMarkerPressed,
   twoMarkerPressed
 }) {
-  const oneRef = useRef(null)
-  const twoRef = useRef(null)
-  const oneWidth = useElementWidth(oneRef, oneMarkerValue)
-  const twoWidth = useElementWidth(twoRef, twoMarkerValue)
-
   return pug`
     Div.root
       // This condition has been taken from original vendor component. Be aware when you change this.
       if Number.isFinite(oneMarkerLeftPosition) && Number.isFinite(oneMarkerValue)
-        Div.label(
-          ref=oneRef
-          style={ left: oneMarkerLeftPosition - oneWidth / 2 }
-        )
+        Div.label(style={ left: oneMarkerLeftPosition - styles.label.width / 2 })
           Span.labelText(styleName={ pressed: oneMarkerPressed })= oneMarkerValue
       // This condition has been taken from original vendor component. Be aware when you change this.
       if Number.isFinite(twoMarkerLeftPosition) && Number.isFinite(twoMarkerValue)
-        Div.label(
-          ref=twoRef
-          style={ left: twoMarkerLeftPosition - twoWidth / 2 }
-        )
+        Div.label(style={ left: twoMarkerLeftPosition - styles.label.width / 2 })
           Span.labelText(styleName={ pressed: twoMarkerPressed })= twoMarkerValue
   `
 }
@@ -49,18 +38,6 @@ Label.propTypes = {
   twoMarkerLeftPosition: PropTypes.number,
   oneMarkerPressed: PropTypes.bool,
   twoMarkerPressed: PropTypes.bool
-}
-
-function useElementWidth (ref, dependency) {
-  const [value, setValue] = useState(0)
-
-  useLayoutEffect(function () {
-    ref.current?.measure(function (x, y, width) {
-      setValue(width)
-    })
-  }, [ref.current, dependency])
-
-  return value
 }
 
 export default Label
