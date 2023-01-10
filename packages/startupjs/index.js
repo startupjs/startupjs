c967b2de9ea9cf7832017b74f949bc0a4a8053ea
Pavel Zhukov <cray0000@gmail.com>
2023-01-10 13:22:54 +0200

    dummy commit to force publish new version

--- packages/startupjs/index.js
+++ packages/startupjs/index.js
@@ -1,7 +1,7 @@
 export * from '@startupjs/react-sharedb'
 export * from '@startupjs/hooks'
 // HINT: `isomorphic` means that the code can be executed both
-//        on the server and on the client
+//        on the server and on the client.
 export * from '@startupjs/isomorphic-helpers'
 // dummy babel macro functions for @startupjs/babel-plugin-rn-stylename-inline
 export function css (cssString) { return cssString }
