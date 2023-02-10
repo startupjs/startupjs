ba2ea52fa16168296496f42a96c3911458e0f9a2
Nariman Beytullaev <nariman.beytullaev@gmail.com>
2023-02-10 16:25:20 +0300

    fix(auth): wrong password attempts, add failedLoginTimestamp

--- packages/auth-local/server/middlewares/setLoginAttempts.js
+++ packages/auth-local/server/middlewares/setLoginAttempts.js
@@ -1,6 +1,7 @@
 import { loginLock } from '@startupjs/auth/server'
 
-const ALLOWED_FAILED_LOGIN_ATTEMPTS = 10
+const ALLOWED_FAILED_LOGIN_ATTEMPTS = 5
+const LIFETIME_FAILED_LOGIN_ATTEMPTS = 3 * 60 * 1000
 
 export default async function setLoginAttempts (req, res, next) {
   const { model, body } = req
@@ -17,16 +18,25 @@ export default async function setLoginAttempts (req, res, next) {
   })
   await $auths.subscribe()
   const authDoc = $auths.get()[0]
+  const now = Date.now()
 
   if (authDoc) {
     const $auth = model.scope(`auths.${authDoc.id}`)
     const failedLoginAttempts = $auth.get('providers.local.failedLoginAttempts') || 0
+    const failedLoginTimestamp = $auth.get('providers.local.failedLoginTimestamp') || 0
 
-    if (failedLoginAttempts > ALLOWED_FAILED_LOGIN_ATTEMPTS) {
+    if (
+      failedLoginTimestamp +
+      LIFETIME_FAILED_LOGIN_ATTEMPTS >
+      now &&
+      failedLoginAttempts % 5 === 0
+    ) {
       await $auth.del('providers.local.failedLoginAttempts')
+      await $auth.del('providers.local.failedLoginTimestamp')
       return loginLock(authDoc.id, req, res)
     } else {
       await $auth.set('providers.local.failedLoginAttempts', failedLoginAttempts + 1)
+      await $auth.set('providers.local.failedLoginTimestamp', now)
     }
   }
 
