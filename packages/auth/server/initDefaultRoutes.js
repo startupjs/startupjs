314fbb2d19921168f6c9ca62140ebe71a9a50d6d
Pavel Turchik <pvturchik@gmail.com>
2023-06-20 13:25:32 +0300

    chore: update 'passport' version

--- packages/auth/server/initDefaultRoutes.js
+++ packages/auth/server/initDefaultRoutes.js
@@ -3,15 +3,18 @@ import { LOGOUT_URL } from '../isomorphic/constants'
 export default function initDefaultRoutes (router, config) {
   const { onBeforeLogoutHook, signInPageUrl } = config
 
-  router.get(LOGOUT_URL, function (req, res) {
+  router.get(LOGOUT_URL, function (req, res, next) {
     onBeforeLogoutHook(
       req,
       res,
       () => {
         delete req.session.loggedIn
         delete req.session.userId
-        req.logout()
-        res.redirect(signInPageUrl)
+
+        req.logout((err) => {
+          if (err) return next(err)
+          res.redirect(signInPageUrl)
+        })
       })
   })
 }
