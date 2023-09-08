f5f8cd3054c4db215504eb4dd4cfe999e8af0c70
Pavel Zhukov <cray0000@gmail.com>
2023-09-08 23:21:31 +0300

    generate default css variables (this is actually not used yet, but in future we should set all default colors into css variables)

--- packages/ui/components/DynamicThemeProvider/defaultUiVariables.js
+++ packages/ui/components/DynamicThemeProvider/defaultUiVariables.js
@@ -1,6 +1,15 @@
 // TODO: Figure out if we would want to transition default UI colors to be dynamic
+import STYLES from './defaultUiVariables.styl'
+
+const { staticColors } = STYLES
 
 export default {
-  '--colors-primary': '#000', // TODO: change
-  '--colors-primaryLightest': '#fff' // TODO: change
+  ...getColors()
+}
+
+function getColors () {
+  return Object.keys(staticColors).reduce((vars, color) => {
+    vars[`--colors-${color}`] = staticColors[color]
+    return vars
+  }, {})
 }
