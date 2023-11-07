77d7dec7dd709ad88b5125ebbda6fce1b4caa005
fcbvirus0k <fcbvirus0k@gmail.com>
2023-11-07 16:05:45 +0300

    fix(ui): restore set of default ui variables

--- packages/ui/Ui.js
+++ packages/ui/Ui.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { pug, observer } from 'startupjs'
-import { ToastProvider } from './components/toast'
-import { DialogsProvider } from './components/dialogs'
+import { setDefaultVariables } from '@startupjs/babel-plugin-rn-stylename-to-style/variables'
+import defaultVariables from './components/CssVariables/defaultUiVariables'
 import Portal from './components/Portal'
+import { DialogsProvider } from './components/dialogs'
+import { ToastProvider } from './components/toast'
 import StyleContext from './StyleContext'
+
+// set default css variables as early as possible
+setDefaultVariables(defaultVariables)
 
 export default observer(function Ui ({ children, style }) {
   return pug`
