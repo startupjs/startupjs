5b47a5abfd6ac042ba6531a5dc44f6270da45880
Dmitry Udavydchenkov <okwergot@gmail.com>
2023-12-04 11:39:55 +0700

    update backend for working with snapshots

--- packages/backend/db.js
+++ packages/backend/db.js
@@ -3,22 +3,12 @@ import ShareDbMingoMemory from 'sharedb-mingo-memory'
 import sqlite3 from 'sqlite3'
 import { mongoClient } from './mongo.js'
 
-const { MONGO_URL, NO_MONGO } = process.env
+const { MONGO_URL, NO_MONGO, DB_PATH, DB_LOAD_SNAPSHOT } = process.env
 
-let db
+async function createSqlDb (filename) {
+  const db = new sqlite3.Database(filename)
 
-if (MONGO_URL && !NO_MONGO) {
-  db = ShareDbMongo(
-    {
-      mongo: callback => callback(null, mongoClient),
-      allowAllQueries: true
-    }
-  )
-} else {
-  db = await new Promise((resolve, reject) => {
-    const db = new sqlite3.Database('sqlite.db')
-    const shareDbMingo = new ShareDbMingoMemory()
-
+  return new Promise((resolve, reject) => {
     db.run(
       'CREATE TABLE IF NOT EXISTS documents (' +
       'collection TEXT, ' +
@@ -28,47 +18,115 @@ if (MONGO_URL && !NO_MONGO) {
       (err) => {
         if (err) return reject(err)
 
-        // Load data from SQLite
-        db.all('SELECT collection, id, data FROM documents', [], (err, rows) => {
-          if (err) return reject(err)
-
-          for (const row of rows) {
-            if (!shareDbMingo.docs[row.collection]) {
-              shareDbMingo.docs[row.collection] = {}
-            }
-            shareDbMingo.docs[row.collection][row.id] = JSON.parse(row.data)
-          }
-
-          console.log('Documents loaded from SQLite to shareDbMingo')
-        })
-
-        // override the commit method to save changes to SQLite
-        const originalCommit = shareDbMingo.commit
-
-        shareDbMingo.commit = function (collection, docId, op, snapshot, options, callback) {
-          originalCommit.call(this, collection, docId, op, snapshot, options, (err) => {
-            if (err) return callback(err)
-
-            db.run(
-              'REPLACE INTO documents (collection, id, data) VALUES (?, ?, ?)',
-              [collection, docId, JSON.stringify(snapshot)],
-              (err) => {
-                if (err) {
-                  console.error(err.message)
-                  return callback(err)
-                }
-
-                console.log(`Document with id ${docId} saved to SQLite`)
-                callback()
-              }
-            )
-          })
-        }
-
-        resolve(shareDbMingo)
+        console.log('DB SQLite was created from file', filename)
+        resolve(db)
       }
     )
   })
 }
+
+async function loadDataToMingo (db, mingo) {
+  return new Promise((resolve, reject) => {
+    db.all('SELECT collection, id, data FROM documents', [], (err, rows) => {
+      if (err) return reject(err)
+
+      for (const row of rows) {
+        if (!mingo.docs[row.collection]) {
+          mingo.docs[row.collection] = {}
+        }
+        mingo.docs[row.collection][row.id] = JSON.parse(row.data)
+      }
+      resolve()
+      console.log('Documents loaded from SQLite to shareDbMingo')
+    })
+  })
+}
+
+async function cloneSqlDb (source, target) {
+  return new Promise((resolve, reject) => {
+    source.serialize(() => {
+      source.all('SELECT * FROM tableName', [], (err, rows) => {
+        if (err) return reject(err)
+
+        // Insert data into target database
+        rows.forEach((row) => {
+          target.run('INSERT INTO tableName (column1, column2) VALUES (?, ?)', [row.column1, row.column2], (err) => {
+            if (err) return reject(err)
+
+            console.log('Documents cloned into SQLite database')
+          })
+        })
+      })
+    })
+  })
+}
+
+// override the commit method to save changes to SQLite
+async function patchMingoCommit (sqlDb, shareDbMingo) {
+  const originalCommit = shareDbMingo.commit
+
+  shareDbMingo.commit = function (collection, docId, op, snapshot, options, callback) {
+    originalCommit.call(this, collection, docId, op, snapshot, options, (err) => {
+      if (err) return callback(err)
+
+      sqlDb.run(
+        'REPLACE INTO documents (collection, id, data) VALUES (?, ?, ?)',
+        [collection, docId, JSON.stringify(snapshot)],
+        (err) => {
+          if (err) {
+            console.error(err.message)
+            return callback(err)
+          }
+
+          console.log(`Document with id ${docId} saved to SQLite`)
+          callback()
+        }
+      )
+    })
+  }
+}
+
+async function initDb () {
+  console.log(DB_PATH, DB_LOAD_SNAPSHOT, MONGO_URL, NO_MONGO)
+
+  if (MONGO_URL && !NO_MONGO) {
+    return ShareDbMongo(
+      {
+        mongo: callback => callback(null, mongoClient),
+        allowAllQueries: true
+      }
+    )
+  }
+
+  console.log(1)
+
+  const shareDbMingo = new ShareDbMingoMemory()
+
+  if (DB_PATH === '' && !DB_LOAD_SNAPSHOT) return shareDbMingo
+
+  console.log(2)
+  const sourcePath = DB_LOAD_SNAPSHOT || DB_PATH || 'sqlite.db'
+  let sqlDb = await createSqlDb(sourcePath)
+  console.log(3, sqlDb)
+  await loadDataToMingo(sqlDb, shareDbMingo)
+  console.log(4)
+  // create new db base on snapshot
+  if (DB_PATH !== '' && DB_LOAD_SNAPSHOT) {
+    const sourceDb = sqlDb
+    console.log(5)
+    sqlDb = await createSqlDb(DB_PATH || 'sqlite.db')
+    console.log(6)
+    await cloneSqlDb(sourceDb, sqlDb)
+  }
+
+  console.log(7)
+  await patchMingoCommit(sqlDb, shareDbMingo)
+
+  return shareDbMingo
+}
+
+const db = await initDb()
+
+console.log(db, 'WTF!!!!!!!!!!!!')
 
 export default db
--- packages/backend/mongo.js
+++ packages/backend/mongo.js
@@ -3,13 +3,15 @@ import { MongoClient } from 'mongodb'
 import isString from 'lodash/isString.js'
 
 const {
+  NO_MONGO,
   MONGO_URL,
   MONGO_OPTS,
   MONGO_SSL_KEY_PATH,
   MONGO_SSL_CERT_PATH,
   MONGO_SSL_CA_PATH
 } = process.env
-
+let mongoClient
+let mongo
 const options = { useUnifiedTopology: true }
 
 if (isString(MONGO_OPTS)) {
@@ -23,8 +25,10 @@ if (isString(MONGO_OPTS)) {
   options.sslCA = fs.readFileSync(MONGO_SSL_CA_PATH)
 }
 
-const mongoClient = new MongoClient(MONGO_URL, options)
-const mongo = mongoClient.db()
+if (MONGO_URL && !NO_MONGO) {
+  mongoClient = new MongoClient(MONGO_URL, options)
+  mongo = mongoClient.db()
+}
 
 function createMongoIndex (collection, keys, options) {
   return mongo.collection(collection).createIndex(keys, options)
