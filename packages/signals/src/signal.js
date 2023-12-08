d61ef78fee9ecec5174726589cf67350b3bc0cbe
Pavel Turchik <pvturchik@gmail.com>
2023-12-08 17:40:17 +0300

    refactor: fix shim for WeakRef and FinalizationRegistry

--- packages/signals/src/signal.js
+++ packages/signals/src/signal.js
@@ -1,6 +1,6 @@
-import { WeakRef, FinalizationGroup } from '@ungap/weakrefs'
 import handlers from './handlers.js'
 import { getRootModel } from './model.js'
+import { WeakRef, FinalizationRegistry } from './shim.js'
 
 export const SEGMENTS = Symbol('path segments')
 export const MODEL = Symbol('scoped model')
@@ -19,7 +19,7 @@ export function isInternalSymbol (symbol) {
 
 const signalsCache = new Map()
 export const __DEBUG_SIGNALS_CACHE__ = signalsCache
-const signalsFinalizationRegistry = new FinalizationGroup(hash => signalsCache.delete(hash))
+const signalsFinalizationRegistry = new FinalizationRegistry(hash => signalsCache.delete(hash))
 
 export function getSignal (segments = [], parentProxyTarget) {
   // when it's a proxyTarget or proxy itself
