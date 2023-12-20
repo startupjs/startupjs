7719e5dd826b7031a3f595af2009c98736e358ed
fcbvirus0k <fcbvirus0k@gmail.com>
2023-12-20 23:16:34 +0300

    fix(docs): remove redundant 'useDarkTheme' hook

--- packages/docs/client/clientHelpers/hooks/useDarkTheme.js
+++
@@ -1,6 +0,0 @@
-import useLocalWithDefault from './useLocalWithDefault'
-const PATH = '_session.Props.darkTheme'
-
-export default function useDarkTheme () {
-  return useLocalWithDefault(PATH, false)
-}
