280801b4275c7daf01976da6a1d4d69a6d1245e6
Pavel Zhukov <cray0000@gmail.com>
2024-01-27 19:53:50 +0200

    fix(bundler/metro-config): plug in startupjs server automatically if it's enabled in startupjs.config.js

--- expoapp/metro.config.js
+++ expoapp/metro.config.js
@@ -1,6 +1,5 @@
 const { getDefaultConfig } = require('startupjs/metro-config')
 const { resolve } = require('path')
-const connect = require('connect')
 
 const config = getDefaultConfig(__dirname)
 
@@ -9,27 +8,5 @@ config.resolver.nodeModulesPaths = [
   resolve(__dirname, 'node_modules'),
   resolve(__dirname, '../node_modules')
 ]
-config.server.enhanceMiddleware = (metroMiddleware) => {
-  return connect()
-    .use(metroMiddleware)
-    .use(getStartupjsMiddleware())
-}
 
 module.exports = config
-
-function getStartupjsMiddleware () {
-  const middleware = (async () => {
-    await import('startupjs/nodeRegister')
-    const { createMiddleware } = await import('startupjs/server')
-    return (await createMiddleware()).middleware
-  })()
-  return function startupjsMiddleware (req, res, next) {
-    (async () => {
-      try {
-        (await middleware).apply(this, arguments)
-      } catch (err) {
-        next(err)
-      }
-    })()
-  }
-}
--- packages/bundler/metro-config.js
+++ packages/bundler/metro-config.js
@@ -1,3 +1,5 @@
+const connect = require('connect')
+
 // To pass existing config for modification, pass it as 'upstreamConfig' in options:
 //   config = getDefaultConfig(__dirname, { upstreamConfig })
 exports.getDefaultConfig = function getDefaultConfig (projectRoot, { upstreamConfig } = {}) {
@@ -17,6 +19,18 @@ exports.getDefaultConfig = function getDefaultConfig (projectRoot, { upstreamCon
         ...(upstreamConfig.resolver.sourceExts || []),
         ...['mjs', 'cjs', 'md', 'mdx', 'css', 'styl', 'svg']
       ]))
+    },
+    server: {
+      ...upstreamConfig.server,
+      // TODO: implement a simple parsing of startupjs.config.js
+      //       to figure out whether we need to load 'server' and 'isomorphic'
+      //       envs or only the 'build' one.
+      //       If $.isomorphic.server is turned off then we don't need the enhanceMiddleware at all
+      enhanceMiddleware: metroMiddleware => {
+        return connect()
+          .use(metroMiddleware)
+          .use(getStartupjsMiddleware())
+      }
     }
   }
 }
@@ -48,5 +62,26 @@ function checkIfExpo (upstreamConfig) {
     return Boolean(path)
   } catch (err) {
     return false
+  }
+}
+
+function getStartupjsMiddleware () {
+  const middlewarePromise = (async () => {
+    await import('./nodeRegister.mjs')
+    await import('@startupjs/registry/loadStartupjsConfig.auto')
+    const { ROOT_MODULE: MODULE } = await import('@startupjs/registry')
+    if (!MODULE.options.server) return
+    const { createMiddleware } = await import('@startupjs/server')
+    return (await createMiddleware()).middleware
+  })()
+  return function startupjsMiddleware (req, res, next) {
+    (async () => {
+      try {
+        const middleware = await middlewarePromise
+        return middleware ? middleware.apply(this, arguments) : next()
+      } catch (err) {
+        return next(err)
+      }
+    })()
   }
 }
