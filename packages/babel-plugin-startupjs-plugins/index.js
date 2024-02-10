2ac427f76a60b2295a3ed46083a9b3d48170ca7a
Pavel Zhukov <cray0000@gmail.com>
2024-02-10 22:39:20 +0200

    implement loading models from the 'model' folder

--- packages/babel-plugin-startupjs-plugins/index.js
+++ packages/babel-plugin-startupjs-plugins/index.js
@@ -1,10 +1,13 @@
 const { statSync } = require('fs')
 const { addDefault } = require('@babel/helper-module-imports')
-const { getRelativePluginImports, getRelativeConfigImport, getConfigFilePaths } = require('./loader')
+const {
+  getRelativePluginImports, getRelativeConfigImport, getConfigFilePaths, getRelativeModelImports
+} = require('./loader')
 
 const VIRTUAL_CONFIG_IMPORT_REGEX = /(?:^|\/)startupjs\.config\.virtual\.js$/
-// const VIRTUAL_MODEL_IMPORT_REGEX = /(?:^|\/)startupjs\.model\.virtual\.js$/
+const VIRTUAL_MODELS_IMPORT_REGEX = /(?:^|\/)startupjs\.models\.virtual\.js$/
 const VIRTUAL_PLUGINS_IMPORT_REGEX = /(?:^|\/)startupjs\.plugins\.virtual\.js$/
+const MODEL_PATTERN_REGEX = /^[a-zA-Z0-9$_*.]+$/
 
 module.exports = function (api, options) {
   const { types: t, template } = api
@@ -33,6 +36,8 @@ module.exports = function (api, options) {
           return loadVirtualConfig($this, { $program, filename, t, root: options.root })
         } else if (isVirtualImport($this, VIRTUAL_PLUGINS_IMPORT_REGEX)) {
           return loadVirtualPlugins($this, { $program, filename, t, template, root: options.root })
+        } else if (isVirtualImport($this, VIRTUAL_MODELS_IMPORT_REGEX)) {
+          return loadVirtualModels($this, { $program, filename, t, template, root: options.root })
         }
       }
     }
@@ -64,10 +69,35 @@ function loadVirtualPlugins ($import, { $program, filename, t, template, root })
     plugins.push(addDefaultImport($program, pluginImport))
   }
 
-  // dummy usage of plugins to make sure they are not removed by dead code elimination
   const pluginsConst = buildPluginsConst({ name, plugins: t.arrayExpression(plugins) })
   const $lastImport = $program.get('body').filter($i => $i.isImportDeclaration()).pop()
   $lastImport.insertAfter(pluginsConst)
+}
+
+function loadVirtualModels ($import, { $program, filename, t, template, root }) {
+  const buildModelsConst = template('const %%name%% = %%models%%')
+
+  // find all models in the project's `models` directory
+  const modelImports = getRelativeModelImports(filename, root)
+
+  const models = t.objectExpression([])
+  for (const modelFilename in modelImports) {
+    const modelPattern = getModelPattern(modelFilename, $import)
+    models.properties.push(t.objectProperty(
+      t.stringLiteral(modelPattern),
+      addDefaultImport($program, modelImports[modelFilename])
+    ))
+  }
+
+  // remove the original magic import
+  validateModelsImport($import)
+  const name = $import.get('specifiers.0.local').node.name
+  $import.remove()
+
+  // dummy usage of plugins to make sure they are not removed by dead code elimination
+  const modelsConst = buildModelsConst({ name, models })
+  const $lastImport = $program.get('body').filter($i => $i.isImportDeclaration()).pop()
+  $lastImport.insertAfter(modelsConst)
 }
 
 // Handle only magic import which imports a magic function
@@ -92,10 +122,33 @@ function addDefaultImport ($program, sourceName) {
 
 function validatePluginsImport ($import) {
   const $specifiers = $import.get('specifiers')
-  if ($specifiers.length === 0 || $specifiers.length > 1) {
-    throw $import.buildCodeFrameError('Expected a single default import')
+  if ($specifiers.length === 0 || $specifiers.length > 1 || !$specifiers[0].isImportDefaultSpecifier()) {
+    throw $import.buildCodeFrameError('Virtual plugins import must have a single default import')
   }
-  if (!$specifiers[0].isImportDefaultSpecifier()) {
-    throw $import.buildCodeFrameError('Expected a single default import')
+}
+
+function validateModelsImport ($import) {
+  const $specifiers = $import.get('specifiers')
+  if ($specifiers.length === 0 || $specifiers.length > 1 || !$specifiers[0].isImportDefaultSpecifier()) {
+    throw $import.buildCodeFrameError('Virtual models import must have a single default import')
   }
+}
+
+function getModelPattern (modelFilename, $import) {
+  let pattern = modelFilename
+  if (/\*/.test(pattern)) {
+    throw $import.buildCodeFrameError('Instead of `*` in model filename use `[id]`. Got: ' + modelFilename)
+  }
+  // replace [id] with *
+  pattern = pattern.replace(/\[[^\]]*\]/g, '*')
+  // remove extension
+  pattern = pattern.replace(/\.[^.]+$/, '')
+  // validate pattern
+  if (!MODEL_PATTERN_REGEX.test(pattern)) {
+    throw $import.buildCodeFrameError(
+      'Invalid model filename pattern: ' + modelFilename + '\n' +
+      'It has to comply with the following regex: ' + MODEL_PATTERN_REGEX.toString() +
+      ' with `[id]` instead of `*`')
+  }
+  return pattern
 }
