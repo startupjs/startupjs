917e506b98dcca460e75dc68efca98afe1b8c4df
Pavel Zhukov <cray0000@gmail.com>
2024-02-11 05:02:34 +0200

    simplify the sample entry server file

---
+++ expoapp/server.sample.js
@@ -0,0 +1,6 @@
+// This is an example server file for production.
+// It's provided just for a reference and is NOT actually being used
+// to start the server.
+// That's because the `package.json/start-production` script runs this internally.
+await import('startupjs/nodeRegister')
+await import('startupjs/startServer')
