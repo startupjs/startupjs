c04efea6700be7528bc67f1c96be42681edbbff4
Pavel Zhukov <cray0000@gmail.com>
2024-02-16 06:39:18 +0200

    remove react-router-native-stack. It wasn't being used anyways.

--- packages/app/client/Router/RoutesWrapper.js
+++ packages/app/client/Router/RoutesWrapper.js
@@ -1,17 +1,3 @@
-import React from 'react'
-import Stack from 'react-router-native-stack'
-import { pug } from 'startupjs'
-const DEFAULT_ANIMATE = false
-// Support for web https://github.com/Traviskn/react-router-native-stack/pull/47
-
-export default function RoutesWrapper ({
-  animate = DEFAULT_ANIMATE,
-  children
-}) {
-  return pug`
-    if animate
-      Stack(gestureEnabled=false animationType='slide-horizontal')= children
-    else
-      = children
-  `
+export default function RoutesWrapper ({ children }) {
+  return children
 }
