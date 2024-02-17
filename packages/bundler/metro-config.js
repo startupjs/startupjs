d24287c09168a02a814d7c15c714a4fa093e53c9
Pavel Zhukov <cray0000@gmail.com>
2024-02-17 21:14:00 +0200

    feat(bundler): add support for 'yarn link'

--- packages/bundler/metro-config.js
+++ packages/bundler/metro-config.js
@@ -1,12 +1,19 @@
 const { getFeatures } = require('@startupjs/babel-plugin-startupjs-plugins/loader')
 const connect = require('connect')
+const { readFileSync } = require('fs')
+const { join, resolve } = require('path')
 
 // To pass existing config for modification, pass it as 'upstreamConfig' in options:
 //   config = getDefaultConfig(__dirname, { upstreamConfig })
 exports.getDefaultConfig = function getDefaultConfig (projectRoot, { upstreamConfig } = {}) {
+  let packageJson = {}
+  try {
+    packageJson = JSON.parse(readFileSync(join(projectRoot, 'package.json'), 'utf8'))
+  } catch (err) {}
   const features = getFeatures(projectRoot)
   upstreamConfig ??= getUpstreamConfig(projectRoot)
   const isExpo = checkIfExpo(upstreamConfig)
+
   const config = {
     ...upstreamConfig,
     transformer: {
@@ -17,22 +24,52 @@ exports.getDefaultConfig = function getDefaultConfig (projectRoot, { upstreamCon
     resolver: {
       ...upstreamConfig.resolver,
       assetExts: upstreamConfig.resolver.assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: Array.from(new Set([
+      sourceExts: [...new Set([
         ...(isExpo ? ['expo.ts', 'expo.tsx', 'expo.js', 'expo.jsx', 'expo.mjs', 'expo.cjs'] : []),
         ...(upstreamConfig.resolver.sourceExts || []),
         ...['mjs', 'cjs', 'md', 'mdx', 'css', 'styl', 'svg']
-      ])),
+      ])],
       unstable_enablePackageExports: true
     }
   }
-  if (features.enableServer) {
-    config.server ??= {}
-    config.server.enhanceMiddleware = metroMiddleware =>
-      connect()
-        .use(metroMiddleware)
-        .use(getStartupjsMiddleware())
-  }
+
+  // Add startupjs server middleware
+  if (features.enableServer) addServer(config)
+
+  // Support Yarn's `resolutions` field from doing `yarn link`
+  if (packageJson.resolutions) addYarnLink(config, { packageJson, projectRoot })
+
   return config
+}
+
+function addServer (config) {
+  config.server ??= {}
+  config.server.enhanceMiddleware = metroMiddleware =>
+    connect()
+      .use(metroMiddleware)
+      .use(getStartupjsMiddleware())
+}
+
+function addYarnLink (config, { packageJson, projectRoot }) {
+  // `yarn link` adds paths with a prefix 'portal:' so we handle only those
+  const linkPaths = Object.values(packageJson.resolutions)
+    .filter(path => path.startsWith('portal:'))
+    .map(path => path.replace(/^portal:/, ''))
+    // paths might be relative to the project root
+    .map(path => resolve(projectRoot, path))
+  if (linkPaths.length === 0) return
+
+  config.watchFolders = [...new Set([
+    ...(config.watchFolders || []),
+    projectRoot,
+    ...linkPaths
+  ])]
+  config.resolver.nodeModulesPaths = [...new Set([
+    ...(config.resolver.nodeModulesPaths || []),
+    // this is supposed to be the default behavior of Metro, but after changing
+    // the watchFolders it stops working for some reason
+    resolve(projectRoot, 'node_modules')
+  ])]
 }
 
 function getUpstreamConfig (projectRoot) {
