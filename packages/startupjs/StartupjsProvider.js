77df5a74315bb942ec723f05e224661a49434a35
Pavel Zhukov <cray0000@gmail.com>
2024-02-24 02:53:40 +0200

    back to using plugin. This is better since it makes 'startupjs' not dependent on react-native by default

--- packages/startupjs/StartupjsProvider.js
+++ packages/startupjs/StartupjsProvider.js
@@ -1,10 +1,6 @@
 import { createElement as el } from 'react'
 import { ROOT_MODULE as MODULE } from '@startupjs/registry'
-import CssMediaUpdater from './CssMediaUpdater'
 
 export default MODULE.dynamicPlugins(function StartupjsProvider ({ children }) {
-  return el(MODULE.RenderNestedHook, { name: 'renderRoot' },
-    el(CssMediaUpdater, null),
-    children
-  )
+  return el(MODULE.RenderNestedHook, { name: 'renderRoot' }, children)
 })
