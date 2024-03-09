6696510fedf121027c2e9b661b7a7b400e4380ad
Pavel Zhukov <cray0000@gmail.com>
2024-03-09 23:07:19 +0200

    feat(ui/Form): run validation right away only if 'validate' is passed. Otherwise you should manually run Form.validate() on Form ref.

--- packages/ui/components/forms/Form/index.js
+++ packages/ui/components/forms/Form/index.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useCallback } from 'react'
+import React, { useMemo, useCallback, useImperativeHandle, useRef } from 'react'
 import { pug, observer, useOn, useValue$ } from 'startupjs'
 import { transformSchema, ajv } from '@startupjs/schema'
 import _set from 'lodash/set'
@@ -18,13 +18,16 @@ function Form ({
   row,
   errors,
   _renderWrapper,
+  validate,
   style,
   inputStyle,
   customInputs = {},
   ...props
-}) {
+}, ref) {
   if (properties) throw Error(ERROR_PROPERTIES)
   const { disabled, readonly, $value } = props
+
+  const shouldValidateRef = useRef()
 
   const memoizedFields = useMemo(
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -33,13 +36,13 @@ function Form ({
 
   if (!$errors) $errors = useValue$() // eslint-disable-line react-hooks/rules-of-hooks
 
-  const validate = useMemo(() => {
+  const runValidation = useMemo(() => {
     let schema = $fields?.get() || memoizedFields
     // we allow extra properties in Form to let people just pass the full document
     // instead of forcing them to pick only the fields used in schema
     schema = transformSchema(schema, { additionalProperties: true })
     return ajv.compile(schema)
-  }, [JSON.stringify(memoizedFields), $fields?.get()])
+  }, [JSON.stringify(memoizedFields), $fields])
 
   const memoizedProps = useMemo(
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -51,23 +54,61 @@ function Form ({
     () => customInputs, [...Object.keys(customInputs), ...Object.values(customInputs)]
   )
 
+  const validateNow = useCallback(() => {
+    if (!shouldValidateRef.current) return
+    const valid = runValidation($value.get())
+    if (valid) {
+      $errors.del()
+      return true
+    }
+    const localErrors = {}
+    for (const error of runValidation.errors) {
+      const path = error.instancePath.replace(/^\//, '').split('/')
+      // handle special case for 'required' fields.
+      // It happens on the level above the field which is actually required
+      // and the actual field name is located in params.missingProperty
+      let message = error.message
+      if (error.keyword === 'required') {
+        if (path.length > 0 && path[path.length - 1] === '') path.pop()
+        path.push(error.params.missingProperty)
+        message = 'This field is required'
+      }
+      if (!_get(localErrors, path)) _set(localErrors, path, [])
+      _get(localErrors, path).push(message)
+    }
+    $errors.setDiffDeep(localErrors)
+    return false
+  }, [runValidation, $value, $errors])
+
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const debouncedValidate = useCallback(
-    _debounce(() => {
-      const valid = validate($value.get())
-      if (valid) return $errors.del()
-      const localErrors = {}
-      for (const error of validate.errors) {
-        const path = error.instancePath.replace(/^\//, '').split('/')
-        if (!_get(localErrors, path)) _set(localErrors, path, [])
-        _get(localErrors, path).push(error.message)
-      }
-      $errors.setDiffDeep(localErrors)
-    }, 30, { leading: false, trailing: true })
-    , [validate, $value, $errors]
+    _debounce(validateNow, 30, { leading: false, trailing: true })
+    , [validateNow]
   )
 
+  useImperativeHandle(ref, () => ({
+    validate: () => {
+      shouldValidateRef.current = true
+      return validateNow()
+    }
+  }), [validateNow])
+
   useOn('all', $value.path() + '.**', debouncedValidate)
+
+  useMemo(() => {
+    // if validate prop is set, trigger validation right away on mount.
+    if (validate) {
+      shouldValidateRef.current = true
+      validateNow()
+    } else {
+      // Otherwise the reactive validation will only start after
+      // Form.validate() is called for the first time.
+      // And we reset $errors on mount to clear any errors that might have been set
+      // from the previous try to submit the form with $errors passed from a parent component.
+      // Even if $errors is passed from a parent component Form itself still fully owns it
+      $errors.del()
+    }
+  }, [])
 
   return pug`
     FormPropsContext.Provider(value=memoizedProps)
@@ -97,7 +138,7 @@ Form.propTypes = {
   $value: PropTypes.any
 }
 
-export default observer(Form)
+export default observer(Form, { forwardRef: true })
 
 const ERROR_PROPERTIES = `
   Form: 'properties' prop can only be used directly in ObjectInput.
