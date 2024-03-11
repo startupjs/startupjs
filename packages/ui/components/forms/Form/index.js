220a766ab99b4b7b852f5cf4affdb210f34a8533
Pavel Zhukov <cray0000@gmail.com>
2024-03-11 20:51:01 +0200

    feat(ui/Form): Simplify api to validate the form - useValidate() and Form(validate) (#1197)
    * feat(ui/Form) Simplify api to validate the form - useValidate() and Form(validate)
    
    * refactor validation logic. Split into Validate (parent) and Validator (child)
    
    * fix reactivity of validate.hasErrors and validate.errors
    
    * bring back user-provided errors support
    
    * bring back passing

--- packages/ui/components/forms/Form/index.js
+++ packages/ui/components/forms/Form/index.js
@@ -1,13 +1,11 @@
-import React, { useMemo, useCallback, useImperativeHandle, useRef } from 'react'
+import React, { useMemo, useCallback, useState, useId } from 'react'
 import { pug, observer, useOn, useValue$ } from 'startupjs'
-import { transformSchema, ajv } from '@startupjs/schema'
-import _set from 'lodash/set'
-import _get from 'lodash/get'
 import _debounce from 'lodash/debounce'
 import PropTypes from 'prop-types'
 import ObjectInput from '../ObjectInput'
 import { CustomInputsContext } from './useCustomInputs'
 import { FormPropsContext } from './useFormProps'
+import { Validator } from './useValidate'
 
 function Form ({
   fields = {},
@@ -23,11 +21,12 @@ function Form ({
   inputStyle,
   customInputs = {},
   ...props
-}, ref) {
+}) {
   if (properties) throw Error(ERROR_PROPERTIES)
   const { disabled, readonly, $value } = props
 
-  const shouldValidateRef = useRef()
+  const formId = useId()
+  const forceUpdate = useForceUpdate()
 
   const memoizedFields = useMemo(
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -35,14 +34,17 @@ function Form ({
   )
 
   if (!$errors) $errors = useValue$() // eslint-disable-line react-hooks/rules-of-hooks
+  const validator = useMemo(() => new Validator(), [])
 
-  const runValidation = useMemo(() => {
-    let schema = $fields?.get() || memoizedFields
-    // we allow extra properties in Form to let people just pass the full document
-    // instead of forcing them to pick only the fields used in schema
-    schema = transformSchema(schema, { additionalProperties: true })
-    return ajv.compile(schema)
-  }, [JSON.stringify(memoizedFields), $fields])
+  useMemo(() => {
+    validator.init({
+      fields: $fields?.get() || memoizedFields,
+      getValue: () => $value.get(),
+      $errors,
+      forceUpdate,
+      formId
+    })
+  }, [JSON.stringify(memoizedFields), $fields, $value, $errors, forceUpdate, formId])
 
   const memoizedProps = useMemo(
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -54,59 +56,28 @@ function Form ({
     () => customInputs, [...Object.keys(customInputs), ...Object.values(customInputs)]
   )
 
-  const validateNow = useCallback(() => {
-    if (!shouldValidateRef.current) return
-    const valid = runValidation($value.get())
-    if (valid) {
-      $errors.del()
-      return true
-    }
-    const localErrors = {}
-    for (const error of runValidation.errors) {
-      const path = error.instancePath.replace(/^\//, '').split('/')
-      // handle special case for 'required' fields.
-      // It happens on the level above the field which is actually required
-      // and the actual field name is located in params.missingProperty
-      let message = error.message
-      if (error.keyword === 'required') {
-        if (path.length > 0 && path[path.length - 1] === '') path.pop()
-        path.push(error.params.missingProperty)
-        message = 'This field is required'
-      }
-      if (!_get(localErrors, path)) _set(localErrors, path, [])
-      _get(localErrors, path).push(message)
-    }
-    $errors.setDiffDeep(localErrors)
-    return false
-  }, [runValidation, $value, $errors])
-
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const debouncedValidate = useCallback(
-    _debounce(validateNow, 30, { leading: false, trailing: true })
-    , [validateNow]
+    _debounce(() => validator.run(), 30, { leading: false, trailing: true }),
+    [validator]
   )
-
-  useImperativeHandle(ref, () => ({
-    validate: () => {
-      shouldValidateRef.current = true
-      return validateNow()
-    }
-  }), [validateNow])
 
   useOn('all', $value.path() + '.**', debouncedValidate)
 
   useMemo(() => {
     // if validate prop is set, trigger validation right away on mount.
-    if (validate) {
-      shouldValidateRef.current = true
-      validateNow()
-    } else {
-      // Otherwise the reactive validation will only start after
-      // Form.validate() is called for the first time.
-      // And we reset $errors on mount to clear any errors that might have been set
-      // from the previous try to submit the form with $errors passed from a parent component.
-      // Even if $errors is passed from a parent component Form itself still fully owns it
-      $errors.del()
+    if (validate === true) {
+      validator.activate()
+      validator.run()
+    } else if (typeof validate === 'function') {
+      validate.reset()
+      validate.init({ validator, formId })
+      if (validate.always) {
+        validator.activate()
+        validator.run()
+      }
+      // when Form is unmounted, reset the parent's validate
+      return () => validate.reset({ formId })
     }
   }, [])
 
@@ -116,7 +87,6 @@ function Form ({
         ObjectInput(
           properties=$fields?.get() || memoizedFields
           $value=$value
-          $errors=$errors
           order=order
           row=row
           errors=errors || $errors.get()
@@ -138,7 +108,12 @@ Form.propTypes = {
   $value: PropTypes.any
 }
 
-export default observer(Form, { forwardRef: true })
+export default observer(Form)
+
+function useForceUpdate () {
+  const [, setState] = useState(Math.random())
+  return useCallback(() => setState(Math.random()), [])
+}
 
 const ERROR_PROPERTIES = `
   Form: 'properties' prop can only be used directly in ObjectInput.
