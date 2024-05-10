37414652020e1345ae9d9e323a1cd5d497532758
Pavel Zhukov <cray0000@gmail.com>
2024-05-10 21:01:41 +0300

    test: test that multiple .set()'s are being batched for the next render

--- packages/signals-orm/test_browser/react.js
+++ packages/signals-orm/test_browser/react.js
@@ -45,19 +45,41 @@ describe('observer', () => {
     expect(container.textContent).toBe('anonymous')
     expect(renders).toBe(1)
   })
+
+  it('batches multiple updates into one render', async () => {
+    const { $name, $surname } = $.session._3
+    let renders = 0
+    const Component = observer(() => {
+      renders++
+      return fr(
+        el('span', {}, $name.get() || 'Anon'),
+        ' ',
+        el('span', {}, $surname.get() || 'Anonymous')
+      )
+    })
+    const { container } = render(el(Component))
+    expect(container.textContent).toBe('Anon Anonymous')
+    expect(renders).toBe(1)
+    act(() => {
+      $name.set('John')
+      $surname.set('Smith')
+    })
+    expect(container.textContent).toBe('John Smith')
+    expect(renders).toBe(2)
+    await new Promise(resolve => setTimeout(resolve, 30))
+    expect(renders).toBe(2)
+  })
 })
 
 describe('use$() hook for creating values', () => {
-  it('creates a value and reuses it on next render', async () => {
+  it('creates a value signal with a default value and reuses it on next render', async () => {
     let renders = 0
     const Component = observer(() => {
       renders++
       const $name = use$('John')
-      return (
-        fr(
-          el('span', {}, $name.get() || 'anonymous'),
-          el('button', { onClick: () => $name.set('Jane') })
-        )
+      return fr(
+        el('span', {}, $name.get() || 'anonymous'),
+        el('button', { onClick: () => $name.set('Jane') })
       )
     })
     const { container } = render(el(Component))
