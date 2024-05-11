bbb343e46d03ed795a4a1573b800a683226d658e
Pavel Zhukov <cray0000@gmail.com>
2024-05-11 22:32:15 +0300

    minor

--- packages/signals-orm/index.js
+++ packages/signals-orm/index.js
@@ -1,3 +1,8 @@
+// NOTE:
+//   $() and sub$() are currently set to be universal ones which work in both
+//   plain JS and React environments. In React they are tied to the observer() HOC.
+//   This is done to simplify the API.
+//   In future, we might want to separate the plain JS and React APIs
 import getSignal from './orm/getSignal.js'
 import { getRootSignal } from './orm/Root.js'
 import { universal$ } from './react/hooks.js'
@@ -8,11 +13,9 @@ export { getSignal as signal }
 export { universalSub$ as sub$ } from './react/hooks.js'
 export const $ = getRootSignal({ rootFunction: universal$ })
 export default $
-export { use$, useSub$ } from './react/hooks.js'
 export { default as observer } from './react/observer.js'
 
-// NOTE:
-//   $() and sub$() are currently set to be universal ones which work in both
-//   plain JS and React environments. In React they are tied to the observer() HOC.
-//   This is done to simplify the API.
-//   In the future, we might want to separate the plain JS and React APIs
+// the following are react-specific hook alternatives to $() and sub$() functions.
+// In future we might want to expose them, but at the current time they are not needed
+// and instead just the regular $() and sub$() functions are used since they are universal
+// export { use$, useSub$ } from './react/hooks.js'
