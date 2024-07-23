f2679c58e96732e53853bb5619b83d79c70924f5
E.Orekhov <oeswww@gmail.com>
2024-07-23 12:07:09 +0300

    fix(auth): fixes checking for enabled plugin with login and logout functions

--- core/server/utils/clientLogout.js
+++ core/server/utils/clientLogout.js
@@ -5,7 +5,7 @@ import { AUTH_PLUGIN_NAME } from './constants.js'
 
 export default async function logout () {
   const plugin = getPlugin(AUTH_PLUGIN_NAME)
-  if (!(plugin.initialized && plugin.initialized)) {
+  if (!plugin.enabled) {
     throw new Error(`Plugin ${AUTH_PLUGIN_NAME} hasn't been initialized`)
   }
   await deleteSessionData()
