2fb10950f9c82f24e8d14b59d4d11b3416530fa0
Pavel Zhukov <cray0000@gmail.com>
2024-10-04 13:28:14 +0300

    feat: support other .env files besides .env.local by providing STAGE=name

--- core/server/dotenv.cjs
+++ core/server/dotenv.cjs
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv')
 
-dotenv.config({ path: ['.env.local', '.env'] })
+dotenv.config({ path: [`.env.${process.env.STAGE || 'local'}`, '.env'] })
 
 module.exports = () => {}
