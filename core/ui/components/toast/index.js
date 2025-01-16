3b048afe80d55d0e8a3645f5eebca91276921feb
vmark <Hikkikomori72@gmail.com>
2025-01-17 00:29:21 +0300

    fix

--- core/ui/components/toast/index.js
+++ core/ui/components/toast/index.js
@@ -8,25 +8,23 @@ const MAX_SHOW_LENGTH = 3
 // Is there a better way to do this?
 // We want to remove unnecessary props from toast
 // component that are added by these calculations.
-const updateMatrixPositions = () => {
-  const toasts = $toasts.get()
+const updateMatrixPositions = (() => {
+  let timeout = null
+  return () => {
+    if (timeout) clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      const toasts = $toasts.get()
+      const updatedToasts = toasts.map((toast, index) => {
+        const prevToast = toasts[index - 1]
+        toast.topPosition = prevToast ? prevToast.topPosition + prevToast.height : 0
+        return toast
+      })
+      $toasts.set(updatedToasts)
+    }, 50)
+  }
+})()
 
-  const updateToasts = toasts.map((toast, index) => {
-    const prevToast = toasts[index - 1]
-
-    if (prevToast) {
-      toast.topPosition = prevToast.topPosition + prevToast.height
-    } else {
-      toast.topPosition = 0
-    }
-
-    return toast
-  })
-
-  $toasts.set(updateToasts)
-}
-
-export default function toast ({
+export default function toast({
   alert,
   icon,
   text,
@@ -42,37 +40,28 @@ export default function toast ({
     $toasts[MAX_SHOW_LENGTH - 1].show.set(false)
   }
 
-  if (!alert) {
-    setTimeout(() => {
-      const index = getValidIndex()
-      if (index !== -1) $toasts[index].show.set(false)
-    }, 5000)
-  }
-
-  function onRemove () {
+  function onRemove() {
     const index = getValidIndex()
-    if (index === -1) return
-
-    $toasts[index].del()
-    updateMatrixPositions()
-    onClose && onClose()
+    if (index !== -1) {
+      $toasts[index].del()
+      updateMatrixPositions()
+      onClose?.()
+    }
   }
 
-  // toastId ensures that the correct index is found at the current moment
-  function getValidIndex () {
+  function getValidIndex() {
     return $toasts.get().findIndex(toast => toast.key === toastId)
   }
 
-  // NOTE
-  // Think about using context instead of model
-  // We can provide registerToast function in context
-  // Which will be better? model or context?
-  function onLayout (layout) {
-    $toasts[getValidIndex()].height.set(layout.height)
-    updateMatrixPositions()
+  function onLayout(layout) {
+    const index = getValidIndex()
+    if (index !== -1) {
+      $toasts[index].height.set(layout.height)
+      updateMatrixPositions()
+    }
   }
 
-  $toasts.unshift({
+  const newToast = {
     key: toastId,
     show: true,
     topPosition: 0,
@@ -85,5 +74,9 @@ export default function toast ({
     onAction,
     onClose: onRemove,
     onLayout
-  })
+  }
+
+  const toasts = $toasts.get() || []
+  toasts.unshift(newToast)
+  $toasts.set(toasts)
 }
