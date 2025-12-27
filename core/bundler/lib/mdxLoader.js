6bc1576008a0bb4b9f568ebd8c84ef8c0e73795d
Pavel Zhukov <cray0000@gmail.com>
2025-12-27 18:43:10 +0200

    feat(bundler): change mdxLoader to use a new @startupjs-ui/mdx package

--- core/bundler/lib/mdxLoader.js
+++ core/bundler/lib/mdxLoader.js
@@ -1,10 +1,15 @@
 const remarkGfm = require('remark-gfm').default
 const { compileSync } = require('@mdx-js/mdx')
+const { readFileSync } = require('fs')
+const path = require('path')
+
+const OLD_MDX_IMPORT = '@startupjs/mdx/useMDXComponents'
+const MDX_IMPORT = '@startupjs-ui/mdx'
 
 module.exports = function getMDXLoader (source) {
   try {
     source = compileSync(source, {
-      providerImportSource: '@startupjs/mdx/useMDXComponents',
+      providerImportSource: getMDXComponentsImport(),
       remarkPlugins: [remarkGfm],
       jsx: true
     }).value
@@ -15,4 +20,23 @@ module.exports = function getMDXLoader (source) {
     }
   }
   return source
+}
+
+let mdxComponentsImport
+// check if the project's package.json has an old @startupjs/ui dependency
+// and use old mdx library in this case for backward compatibility
+function getMDXComponentsImport () {
+  if (mdxComponentsImport) return mdxComponentsImport
+  let hasOldStartupUi = false
+  try {
+    const projectPkgPath = path.join(process.cwd(), 'package.json')
+    const packageJson = readFileSync(projectPkgPath, 'utf8')
+    if (/"@startupjs\/ui"/.test(packageJson)) hasOldStartupUi = true
+  } catch (err) {}
+  if (hasOldStartupUi) {
+    mdxComponentsImport = OLD_MDX_IMPORT
+  } else {
+    mdxComponentsImport = MDX_IMPORT
+  }
+  return mdxComponentsImport
 }
